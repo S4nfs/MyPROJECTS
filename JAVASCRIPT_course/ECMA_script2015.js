c20349cebc39de8e827913281423e993952e4819
s4nfs <zyzzaestheticsgods@gmail.com>
2021-12-03 21:20:23 +0530

    updated

---
+++ JAVASCRIPT_course/ECMA_script2015.js
@@ -0,0 +1,42 @@
+// ✔️ LET VS CONST  vs VAR--------------------------------------------------------------------------------------------------
+// var myname = "Sagar Verma";
+// myname = "S4nfs";
+// console.log(myname);                       //output: S4nfs                     
+
+// let myname1 = "Sagar Verma";
+// myname1 = "S4nfs";
+// console.log(myname1);                      //output: S4nfs                     
+
+// const myname2 = "Sagar Verma";             //error: const variable can't be changed
+// myname2 = "S4nfs";
+// console.log(myname2);
+
+
+// ✔️ Var => Function Scope  | let and const => Block Scope--------------------------------------------------------------------
+// function biodata(){
+// var myFirstName = "Sagar";                   //with var
+// console.log(myFirstName);
+
+// if(true){
+//     var myLastName = "Verma";
+//     console.log('inner ' + myLastName);
+//     console.log('inner ' + myFirstName);
+// }
+// console.log('innerouter ' + myLastName);
+
+// }
+// biodata();
+
+// function biodata(){
+//     let myFirstName = "Sagar";                  //with let
+//     console.log(myFirstName);
+    
+//     if(true){
+//         let myLastName = "Verma";
+//         console.log('inner ' + myLastName);
+//         console.log('inner ' + myFirstName);
+//     }
+//     console.log('innerouter ' + myLastName);    //error: you can't use let variables outside function block
+    
+//     }
+//     biodata();
---
+++ JAVASCRIPT_course/functions.js
@@ -0,0 +1,42 @@
+function sum(){
+    var a = 10, b = 20;
+    var total = a + b; 
+    console.log(total);
+}
+sum();
+
+// ---------------------------------------------------------------------------------------------------------------------------
+
+//function parameters are tthe names listed in function's definitions
+//function parameters are the real values passed to the functions
+
+function sum(a, b){         //parameters
+    var total = a + b; 
+    console.log(total);
+}
+sum(20, 30);                //Arguments
+
+
+//function expression means creating a function and put it into a variable-----------------------------------------------------
+
+function sum(a , b){
+    var total = a + b;
+    console.log(total);
+}
+var funcExp = sum(50,50);     //Expression (but here vaiable funcExp is undefined becoz value is stil in $total so we use return)
+console.log("The sum of total number is : " + funcExp);
+
+//Return simply return backs the value to caller---------------------------------------------------------------------------------
+
+function sum(a , b){
+    return total = a + b;
+}
+var funcExp = sum(50,50);                    
+console.log("The sum of total number is : " + funcExp);
+
+
+//Anonnymous Function (function with no name ex. libvirt demo3 wala)--------------------------------------------------------------
+var funcExp = function(a , b){
+    return total = a + b;
+}
+console.log("The sum of total number is : " + funcExp(5,15));
