357ed86808fb4736fe3dc58ded70fe575d4ccfcf
S4nfs <zyzzaestheticsgods@gmail.com>
2021-12-07 22:09:21 +0530

    updated

--- JAVASCRIPT_course/arrays(withFor-in&For-off_Loops).js
+++ JAVASCRIPT_course/arrays(withFor-in&For-off_Loops).js
@@ -20,14 +20,26 @@ var myFriends = ["Sagar","Pratik","Anjali"];
 //     console.log(elements);
 // }
 
-myFriends.forEach(function(element, index, array){  //ForEach is just a combination of For-in & For-of loop
-    console.log(element)
-})
+// myFriends.forEach(function(element, index, array){  //ForEach is just a combination of For-in & For-of loop
+//     console.log(element)
+// })
 
 
 // ✔️ Searching $ Filter in an arrays ---------------------------------------------------------------------------
-var myFriendz = ["Sagar","Pratik","Anjali","Pratham","Devendra"];
-console.log(myFriendz.indexOf("Pratham"));          //indexof = returns index position
-console.log(myFriendz.includes("Pratham"));         //incudes = check if the element exists and returns true
+// var myFriendz = ["Sagar","Pratik","Anjali","Pratham","Devendra"];
+// console.log(myFriendz.indexOf("Pratham"));          //indexof = returns index position
+// console.log(myFriendz.includes("Pratham"));         //incudes = check if the element exists and returns true
+
+
+
+const prices = [200,300,350,400,450,500,600];
+const findElementoz = prices.find((currVal) => {       //find = problem is that it only returns one element, here 200
+return currVal < 400;
+});
+
+console.log(findElementoz);
+
+
+
 
 
