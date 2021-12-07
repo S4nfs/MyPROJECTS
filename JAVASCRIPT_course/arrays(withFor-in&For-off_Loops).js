8f9ea84451029858915504ea7dbdb0eea2ddcd5d
S4nfs <zyzzaestheticsgods@gmail.com>
2021-12-07 20:54:09 +0530

    updated

--- JAVASCRIPT_course/arrays(withFor-in&For-off_Loops).js
+++ JAVASCRIPT_course/arrays(withFor-in&For-off_Loops).js
@@ -6,3 +6,16 @@
 var myFriends = ["Sagar","Pratik","Anjali"];
 console.log(myFriends[0]);
 console.log(myFriends.length);
+
+for(i=0; i<myFriends.length; i++){
+    console.log(myFriends[i]);
+}
+
+// ✔️ For-in & For-of Loops only in arrays ------------------------------------------------------------------------------------
+for(let elements in myFriends){             //return index number
+    console.log(elements);
+}
+
+for(let elements of myFriends){             //return array elements
+    console.log(elements);
+}
