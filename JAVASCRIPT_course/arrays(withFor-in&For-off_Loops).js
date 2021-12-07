677806335c9293aebb640bef3ba8a7a0fab72310
S4nfs <zyzzaestheticsgods@gmail.com>
2021-12-07 21:11:58 +0530

    updated

--- JAVASCRIPT_course/arrays(withFor-in&For-off_Loops).js
+++ JAVASCRIPT_course/arrays(withFor-in&For-off_Loops).js
@@ -1,21 +1,25 @@
-// ✔️Arrays
+// ✔️Arrays--------------------------------------------------------------------------------------------------------------------
 // var myfriends = new Array; //optional
 // var myfriends = ["Sagar", 30, "Male", "Pratik", 29, "Male", "Anjali", 28, "Female"]; //you can put anything in javascript array
 
 
 var myFriends = ["Sagar","Pratik","Anjali"];
-console.log(myFriends[0]);
-console.log(myFriends.length);
+// console.log(myFriends[0]);
+// console.log(myFriends.length);
 
-for(i=0; i<myFriends.length; i++){
-    console.log(myFriends[i]);
-}
+// for(i=0; i<myFriends.length; i++){
+//     console.log(myFriends[i]);
+// }
 
-// ✔️ For-in & For-of Loops only in arrays ------------------------------------------------------------------------------------
-for(let elements in myFriends){             //return index number
-    console.log(elements);
-}
+// ✔️ For-in & For-of & ForEach Loops only in arrays ---------------------------------------------------------------------------
+// for(let elements in myFriends){                  //return index number
+//     console.log(elements);
+// }
 
-for(let elements of myFriends){             //return array elements
-    console.log(elements);
-}
+// for(let elements of myFriends){                  //return array elements
+//     console.log(elements);
+// }
+
+myFriends.forEach(function(element, index, array){  //ForEach is just a combination of For-in & For-of loop
+    console.log(element)
+})
