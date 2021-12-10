1d0917ed953be6ca5067d221ae4c4ff3f628b526
S4nfs <zyzzaestheticsgods@gmail.com>
2021-12-10 22:33:50 +0530

    updated

---
+++ JAVASCRIPT_course/DOM_BOM.js
@@ -0,0 +1,4 @@
+/*✔️
+BOM(Browser Object Model)       :within Window like history, location, screen, navigator, frames, XMLHttpRequest
+DOM(Document Object Modal)      :within Document ..used in events later
+*/
---
+++ JAVASCRIPT_course/date_time.js
@@ -0,0 +1,10 @@
+/*✔️Date objects 4 :
+    -new Date()
+    -new Date(year, month, day, hours, minutes, seconds, milliseconds)
+    -new Date(milliseconds)
+    -new Date(date string)
+*/
+
+var currDate = new Date();
+console.log(currDate);
+console.log(new Date().toLocaleString());               //local time
