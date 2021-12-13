7ff1933df02a1d948aece7b761320033095f92eb
S4nfs <zyzzaestheticsgods@gmail.com>
2021-12-13 21:44:47 +0530

    updated

--- JAVASCRIPT_course/BOM_DOM.js
+++ JAVASCRIPT_course/BOM_DOM.js
@@ -1,4 +1,4 @@
-/*✔️
+/*✔️ see BOM_DOM.png
 BOM(Browser Object Model)       :within Window like history, location, screen, navigator, frames, XMLHttpRequest
 DOM(Document Object Modal)      :within Document ..used in events later
 */
