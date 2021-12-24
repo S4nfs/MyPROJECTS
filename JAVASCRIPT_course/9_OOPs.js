887e9fa9201d8fad6bf6e2b550c4316fae83b97d
S4nfs <zyzzaestheticsgods@gmail.com>
2021-12-24 21:11:00 +0530

    updated

--- JAVASCRIPT_course/9_OOPs.js
+++ JAVASCRIPT_course/9_OOPs.js
@@ -7,24 +7,40 @@ Object = School bag
 
 
 //✔️How to create an object (that include both variables and function)?
-let bioData = {
- // Key    : "Value",
-    myname : "Sagar",
-    myage  : 30,
-    getData (){                      //if a function is call inside an object we call it now "method" usually
-console.log(`My name is ${bioData.myname} and my age is ${bioData.myage}`);
-    }
-}
-bioData.getData();
+// let bioData = {
+//  // Key    : "Value",
+//     myname : "Sagar",
+//     myage  : 30,
+//     getData (){                      //if a function is call inside an object we call it now "method" usually
+// console.log(`My name is ${bioData.myname} and my age is ${bioData.myage}`);
+//     }
+// }
+// bioData.getData();
 
 
 //✔️What if we want an object as a vale inside an object ?
-let bioData = {
-        whoAmI : 
-       myname : "Sagar",
-       myage  : 30,
-       getData (){                     
-   console.log(`My name is ${bioData.myname} and my age is ${bioData.myage}`);
-       }
+// let bioData = {
+//         whoAmI : {
+//             realName : "S4nfs",
+//             companyName : "Chatnaut"
+//         },
+//         myname : "Sagar",
+//         myage  : 30,
+//        getData (){                     
+//    console.log(`My name is ${bioData.myname} and my age is ${bioData.myage}`);
+//        }
+//    }
+//    bioData.getData();
+//    console.log(bioData.whoAmI.companyName);
+
+
+/*✔️What is (this) object ?
+Note:(this) object will not work wth fat arrow function
+*/
+   var me = "Sagar";
+   function myName() {
+       console.log(this.me);
    }
-   bioData.getData();
+   myName();                                                        //run on browser
+
+   
