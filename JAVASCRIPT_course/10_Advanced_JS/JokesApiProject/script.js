c0d06f131c94a3772c55613b1aaa4454c7259e1f
S4nfs <zyzzaestheticsgods@gmail.com>
2021-12-27 18:48:26 +0530

    updated

--- JAVASCRIPT_course/10_Advanced_JS/JokesApiProject/script.js
+++ JAVASCRIPT_course/10_Advanced_JS/JokesApiProject/script.js
@@ -5,14 +5,14 @@
 const jokes = document.querySelector('#joke');
 const jokeBtn = document.querySelector('#jokeBtn');
 
-
+//using PROMISES-----------------------------------------------------------------------------------------------------------------
 const generateJokes = () => {
 const setHeader = {
     headers: {
         Accept: 'application/json' 
     }
 }
-    fetch('https://icanhazdadjoke.com/', setHeader)                                //using PROMISES
+    fetch('https://icanhazdadjoke.com/', setHeader)                                
     .then((responseMila) => responseMila.json())
     .then((data) => {
      jokes.innerHTML = data.joke;  
@@ -25,3 +25,24 @@ const setHeader = {
 }
 jokeBtn.addEventListener('click', generateJokes);
 generateJokes();
+
+
+//using Async Awaits-----------------------------------------------------------------------------------------------------------------
+
+// const generateJokes = async () => {
+//     try{
+//         const setHeader = {
+//             headers: {
+//                 Accept: 'application/json' 
+//             }
+//         }
+//         const responseMila = await fetch('https://icanhazdadjoke.com/', setHeader);
+//         const data = await responseMila.json();
+//         jokes.innerHTML = data.joke;
+//     }
+//     catch(err){
+//         console.log(`The error is ${err}`);
+//     }
+// }
+// jokeBtn.addEventListener('click', generateJokes);
+// generateJokes();
