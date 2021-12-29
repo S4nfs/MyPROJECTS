21c8160a481e375fe9b3458564032334939a7109
S4nfs <zyzzaestheticsgods@gmail.com>
2021-12-30 00:05:03 +0530

    updated

--- JAVASCRIPT_course/10_Advanced_JS/GoogleKeepCloneProject/script.js
+++ JAVASCRIPT_course/10_Advanced_JS/GoogleKeepCloneProject/script.js
@@ -3,9 +3,26 @@ const addbutton = document.querySelector('#add');
 const addNewNote = (text = '') => {
 const note = document.createElement('div');
 note.classList.add('note');
-const htmlData  = `
+const htmlData  = `;
+<div class="operation">
+<button class="edit"><i class="fas fa-edit"></i></button>
+<button class="delete"><i class="fas fa-trash-alt"></i></button>
+</div>
+<div class="main"></div>
+<textarea class=""></textarea>
+`;
 
+note.insertAdjacentHTML('afterbegin', htmlData);
+console.log(note);
 
-`
+/*Visualization of poition names
+----beforebegin----
+<div>   </div>
+----afterbegin----
+foo
+----beforeend----
+<div>   </div>
+----afterend----
+*/
 }
 addbutton.addEventListener('click', () => addNewNote() );
