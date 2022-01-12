d13896ae8bd1e9d3a414f8cb4f86bf80c4d67e61
S4nfs <zyzzaestheticsgods@gmail.com>
2022-01-12 23:11:02 +0530

    updated

--- JAVASCRIPT_course/10_Advanced_JS/SpotifyClone/script.js
+++ JAVASCRIPT_course/10_Advanced_JS/SpotifyClone/script.js
@@ -4,16 +4,22 @@ let audioElement = new Audio("songs/1.mp3");
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif = document.getElementById('gif');
+let songItems = Array.from(document.getElementsByClassName('songItem'));        
 
 let songs = [
-    {songName: "Jump", filepath: "songs/1.mp3", cover: "covers/1.jpg"},
-    {songName: "Jump", filepath: "songs/2.mp3", cover: "covers/2.jpg"},
-    {songName: "Jump", filepath: "songs/3.mp3", cover: "covers/3.jpg"},
-    {songName: "Jump", filepath: "songs/4.mp3", cover: "covers/4.jpg"},
-    {songName: "Jump", filepath: "songs/5.mp3", cover: "covers/5.jpg"},
-
+    {songName: "Jump", filepath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Bailando", filepath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "Thrills", filepath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "We Don't Care", filepath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Fire", filepath: "songs/5.mp3", coverPath: "covers/5.jpg"},
 ]
 
+songItems.forEach((element, i) => {
+    // console.log(element, i);
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementByClassName("songName")[0].innerText = songs[i].songName;
+    
+})
 //  Play||Pause the Song
 masterPlay.addEventListener('click', () => {
     if(audioElement.paused || audioElement.currentTime <=0){
@@ -38,4 +44,10 @@ audioElement.addEventListener('timeupdate', () => {
 
 myProgressBar.addEventListener('change', () => {
     audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
+})
+
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+    element.addEventListener('click', (e) => {
+        console.log(e.target);
+    })
 })
