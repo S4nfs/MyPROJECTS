07bba89c763ae2ed8340566785ac8f1047983141
elondust <sagar2nfs@gmail.com>
2022-01-12 20:18:54 +0530

    updated

---
+++ JAVASCRIPT_course/10_Advanced_JS/SpotifyClone/script.js
@@ -0,0 +1,35 @@
+//Initialize the Variables
+
+let songs = [
+    {
+        songName: "Jump",
+        filepath: "songs/ATC.mp3",
+        cover: "images/coverATC.jpg",
+    }
+    {
+        songName: "Jump",
+        filepath: "songs/ATC.mp3",
+        cover: "images/coverATC.jpg",
+    }
+    {
+        songName: "Jump",
+        filepath: "songs/ATC.mp3",
+        cover: "images/coverATC.jpg",
+    }
+    {
+        songName: "Jump",
+        filepath: "songs/ATC.mp3",
+        cover: "images/coverATC.jpg",
+    }
+    {
+        songName: "Jump",
+        filepath: "songs/ATC.mp3",
+        cover: "images/coverATC.jpg",
+    }
+]
+
+let audioElement = new Audio('ATC.mp3');
+//audioElement.play();
+
+//Listen to Events
+document.addEventListener('time');
