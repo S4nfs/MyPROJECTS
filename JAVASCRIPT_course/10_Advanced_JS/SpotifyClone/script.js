10d943ecb5fe0815ed3dfc26fdd3d2e8d0200a91
S4nfs <zyzzaestheticsgods@gmail.com>
2022-01-12 22:27:40 +0530

    updated

--- JAVASCRIPT_course/10_Advanced_JS/SpotifyClone/script.js
+++ JAVASCRIPT_course/10_Advanced_JS/SpotifyClone/script.js
@@ -1,43 +1,41 @@
 //Initialize the Variables
 let songIndex = 0;
-let audioElement = new Audio('ATC.mp3');
+let audioElement = new Audio("songs/1.mp3");
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+
 let songs = [
-    {
-        songName: "Jump",
-        filepath: "songs/ATC.mp3",
-        cover: "images/coverATC.jpg",
-    }
-    {
-        songName: "Jump",
-        filepath: "songs/ATC.mp3",
-        cover: "images/coverATC.jpg",
-    }
-    {
-        songName: "Jump",
-        filepath: "songs/ATC.mp3",
-        cover: "images/coverATC.jpg",
-    }
-    {
-        songName: "Jump",
-        filepath: "songs/ATC.mp3",
-        cover: "images/coverATC.jpg",
-    }
-    {
-        songName: "Jump",
-        filepath: "songs/ATC.mp3",
-        cover: "images/coverATC.jpg",
-    }
+    {songName: "Jump", filepath: "songs/1.mp3", cover: "covers/1.jpg"},
+    {songName: "Jump", filepath: "songs/2.mp3", cover: "covers/2.jpg"},
+    {songName: "Jump", filepath: "songs/3.mp3", cover: "covers/3.jpg"},
+    {songName: "Jump", filepath: "songs/4.mp3", cover: "covers/4.jpg"},
+    {songName: "Jump", filepath: "songs/5.mp3", cover: "covers/5.jpg"},
+
 ]
 
-//audioElement.play();
-
 //  Play||Pause the Song
+masterPlay.addEventListener('click', () => {
+    if(audioElement.paused || audioElement.currentTime <=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-pause-circle');
+        masterPlay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
 
 //  Listen to Events
-myProgressBar.addEventListener('timeupdate', () => {
-    console.log('timeeupdate');
+audioElement.addEventListener('timeupdate', () => {
     //Update the Progress Bar
+    progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+    myProgressBar.value = progress;
+})
 
-});
+myProgressBar.addEventListener('change', () => {
+    audioElement.currentTime = myProgressBar.value * audioElement.duration / 100;
+})
