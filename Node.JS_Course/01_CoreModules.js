1c7e9f2a30b850f07722031ce3d61ba511732122
S4nfs <zyzzaestheticsgods@gmail.com>
2022-01-22 21:26:37 +0530

    node.js course filesystem

--- Node.JS_Course/01_CoreModules.js
+++ Node.JS_Course/01_CoreModules.js
@@ -18,11 +18,22 @@ const fs = require("fs"); //file system
 /*Challenge Time Create CRUD operations on a file system
 1.Create a folder called "NodeJSFileSystemCRUD"
 2.Create a file called "Data.txt" inside it and write "Hello World"
-3.Add more data into the file at the end of th exixting data
+3.Add more data into the file at the end of the existing data
 4.Read the data withour getting the buffer data
 5.Rename the file to "Data_new.txt"
 6.Delete both file and folder
 */
 
-// fs.mkdirSync("Node.JS_Course/NodeJSFileSystemCRUD");
-fs.writeFileSync("Node.JS_Course/NodeJSFileSystemCRUD/Data.txt", "Hello World");
+// fs.mkdirSync("Node.JS_Course/NodeJSFileSystemCRUD");                       
+
+// fs.writeFileSync("Node.JS_Course/NodeJSFileSystemCRUD/Data.txt", "Hello World");
+
+// fs.appendFileSync("Node.JS_Course/NodeJSFileSystemCRUD/Data.txt", " I am learning Node.js");
+
+// const mybufferdata = fs.readFileSync("Node.JS_Course/NodeJSFileSystemCRUD/Data.txt" , "utf8");
+// console.log(mybufferdata);
+
+// fs.renameSync("Node.JS_Course/NodeJSFileSystemCRUD/Data.txt", "Node.JS_Course/NodeJSFileSystemCRUD/Data_new.txt");
+
+// fs.unlinkSync("Node.JS_Course/NodeJSFileSystemCRUD/Data_new.txt");
+// fs.rmdirSync("Node.JS_Course/NodeJSFileSystemCRUD");
