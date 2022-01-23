2efba11a8b28cf6b814790f17d1558633af49097
S4nfs <zyzzaestheticsgods@gmail.com>
2022-01-23 20:42:12 +0530

    Bootstrap Growl Like Notification added

---
+++ BootstrapNotifier/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <title>Hello, world!</title>
+  </head>
+  <body>
+<?php
+$notification =  "Snapshot forsuccessfully created";
+
+?>
+    
+<?php
+include('footer.php');
+?>
+
+</body>
+</html>
+
+
