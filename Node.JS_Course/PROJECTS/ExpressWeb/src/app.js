d8d32d1478e067b4cf55747e2ccc692eaec84592
S4nfs <zyzzaestheticsgods@gmail.com>
2022-02-08 14:20:22 +0530

    Express Web Weather page created, routing done

--- Node.JS_Course/PROJECTS/ExpressWeb/src/app.js
+++ Node.JS_Course/PROJECTS/ExpressWeb/src/app.js
@@ -2,27 +2,36 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 8002;
+const hbs = require('hbs');
 
 // public static path
 const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
 
+app.set('view engine', 'hbs');
+app.set('views', template_path);
 app.use(express.static(static_path));
+hbs.registerPartials(partials_path);   
+// Note: In order to reflect changes in any other files LIVE run=> nodemon .\src\app.js -e js,hbs
 
 //routing
 app.get("/", (req, res) => {
-  res.send("Home Page");
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.send("About Page");
+  res.render("about");
 });
 
 app.get("/weather", (req, res) => {
-  res.send("Weather Page");
+  res.rrender("weather");
 });
 
 app.get("*", (req, res) => {
-  res.send("404 Error Pge");
+  res.render("404error", {
+    errormsg: "OOPS!! PAGE NOT FOUND !!"
+  });
 });
 
 app.listen(port, () => {
