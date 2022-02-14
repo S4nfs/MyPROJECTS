be3d700d05b9c8b95141049765b38aa4d0c8b1ff
S4nfs <zyzzaestheticsgods@gmail.com>
2022-02-14 22:26:41 +0530

    let,var,const explained

---
+++ JAVASCRIPT_course/2_Let,Var,Const.js
@@ -0,0 +1,26 @@
+//Example: 1 Hoisting 🍄-----------------------------------
+//Hoisting is JavaScript's default behavior of moving declarations to the top. Var is hoisted but let is not
+"use strict";
+function receive(name = "") {
+    if (name == "Sagar") {
+       var x = "ok";                    //Initialization
+    } else {
+       var x = "not ok";
+    }
+    var x;    // x is hoisted to the top //Declaration
+    console.log(x)
+}
+receive("Sagar");
+
+//Example: 2 Scope 🍄--------------------------------------
+var a = 1;
+let b = 2;
+
+if (a == 1) {
+    var a = 10;
+    let b = 20;
+    console.log(a); //10
+    console.log(b); //20 but its block scoped
+}
+console.log(a); //10
+console.log(b); //2
