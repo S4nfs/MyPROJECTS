686a633120228ebbd7e5daa85835106025831653
S4nfs <zyzzaestheticsgods@gmail.com>
2022-02-14 22:27:55 +0530

    python course updated introduction part completed

---
+++ PYTHON_course/03_Input.py
@@ -0,0 +1,12 @@
+# a = input("Enter Your Name: ")  #take input as a string even its a number
+# print("Hello", a) 
+
+
+# Take input and return average of two numbers
+
+b = input("Enter First Number: ")
+c = input("Enter Second Number: ")
+b  = int(b)
+c = int(c)
+avg = (b+c)/2
+print("Average of two numbers is: ", avg)
---
+++ PYTHON_course/04_String.py
@@ -0,0 +1,13 @@
+greeting = "Good Morning, "
+group = "to all of you"
+print(greeting + group) #concatenation
+
+# 🍏 String Slicing-------------------------------------------------------
+
+name = "John"
+print(name[0])  #J
+print(name[0:3]) #Joh
+print(name[0:]) #John
+print(name[:]) #John
+print(name[-1]) #n
+print(name[-4]) #J
