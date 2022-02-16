6e7c4a561de3b6abcabf804ce8718faa048e92c1
S4nfs <zyzzaestheticsgods@gmail.com>
2022-02-16 20:56:45 +0530

    Conditions in Python

---
+++ PYTHON_course/09_If_Conditions.py
@@ -0,0 +1,14 @@
+a = 11
+if(a > 14):
+    print("Value is greater than 14")
+elif(a > 5):
+    print("Value is greater than 5")
+else:
+    print("Value of both the no. is below 11")
+
+#------------------------------------------------------
+b = int(input("Enter your age: \n"))
+if b>=18:
+    print("yes")
+else:
+    print("No")
