bd6d2667926620b9a96adbbb1d1917ffea11692d
S4nfs <zyzzaestheticsgods@gmail.com>
2022-02-18 21:36:28 +0530

    Identity & Membership Operators added

--- PYTHON_course/02_Introduction.py
+++ PYTHON_course/02_Introduction.py
@@ -20,6 +20,8 @@ print(type(e)) #<class 'NoneType'>
 3. Comparison Operators : ==, !=, >, <, >=, <=
 4. Logical Operators : and, or, not
 5. Bitwise Operators : &, |, ^, ~, <<, >>
+6. Membership Operators : in, not in
+7. Identity Operators : is, is not
 
 '''
 #[1]
--- PYTHON_course/09_If_Conditions.py
+++ PYTHON_course/09_If_Conditions.py
@@ -6,9 +6,34 @@ elif(a > 5):
 else:
     print("Value of both the no. is below 11")
 
-#------------------------------------------------------
-b = int(input("Enter your age: \n"))
-if b>=18:
-    print("yes")
+# -----------------------------------------------------------------------
+# b = int(input("Enter your age: \n"))
+# if b >= 18:
+#     print("yes")
+# else:
+#     print("No")
+
+
+# 🍏 With Logical Operators----------------------------------------------
+# age = int(input("Enter you age: \n"))
+
+# if(age > 60 and age < 90):
+#     print("Getting Senior Citizen Benefits")
+    
+# else:
+#     print("Not Eligible")
+
+# 🍏 With Identity Operators----------------------------------------------
+# d = "dog"
+# if(d is "dog"):
+#     print("dog")
+# else:
+#     print("Not dog")
+
+
+# 🍏 With Membership Operators----------------------------------------------
+a = [1,2,3,4,5]
+if(1 in a):
+    print("1 is in a")
 else:
-    print("No")
+    print("1 is not in a")
