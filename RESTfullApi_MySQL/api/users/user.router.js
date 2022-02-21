6b5d7d0641d3de6fded26259a0068432f7657d44
S4nfs <zyzzaestheticsgods@gmail.com>
2022-02-21 22:13:22 +0530

    sending post data using fetch API promise

--- RESTfullApi_MySQL/api/users/user.router.js
+++ RESTfullApi_MySQL/api/users/user.router.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const { checkToken } = require("../../auth/token_validation");
 
 router.post("/", checkToken, createUser);
-router.get("/", checkToken, getUsers);
+router.get("/", getUsers);
 router.get("/:id", checkToken, getUserByUserId);
 router.patch("/", checkToken, updateUser);
 router.delete("/", checkToken, deleteUser);
