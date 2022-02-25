0badce0ab09107dff605b4f538fa8317741956bc
S4nfs <zyzzaestheticsgods@gmail.com>
2022-02-25 15:28:36 +0530

    fixed CORS block

--- RESTfullApi_MySQL/app.js
+++ RESTfullApi_MySQL/app.js
@@ -1,10 +1,13 @@
 require("dotenv").config();                 // hide sensitive information to environment variables
 
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 // const port = process.env.PORT || 3001    // defined in .env
-const userRouter = require("./api/users/user.router");
 app.use(express.json());
+app.use(cors());
+const userRouter = require("./api/users/user.router");
 
 app.use("/api/users", userRouter);
 app.listen(process.env.APP_PORT, () => {
