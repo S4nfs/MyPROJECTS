72a81b682aeb46db884427d50d39f1cbcd6e7eca
S4nfs <zyzzaestheticsgods@gmail.com>
2022-02-28 20:29:25 +0530

    MErN login form

--- Node.JS_Course/PROJECTS/MErN_loginform/src/app.js
+++ Node.JS_Course/PROJECTS/MErN_loginform/src/app.js
@@ -2,14 +2,25 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
+const hbs = require('hbs');
 require('./db/conn');
 
 const static_path = path.join(__dirname, '../public')
+const template_path = path.join(__dirname, '../templates/views')
+const partials_path = path.join(__dirname, '../templates/partials')
+
 
 app.use(express.static(static_path));
+app.set("view engine", 'hbs');
+app.set('views', template_path);
+hbs.registerPartials(partials_path);
 
 app.get('/', (req, res) => {
-    res.send("hello");
+    res.render('index');
+});
+
+app.get('/register', (req,res) => {
+    res.render('register')
 });
 
 app.listen(port, () => {
