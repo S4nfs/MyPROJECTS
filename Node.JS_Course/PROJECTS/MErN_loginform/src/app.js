28abb55b6a4d37a9e5016dde2127191e7601a929
S4nfs <zyzzaestheticsgods@gmail.com>
2022-03-01 20:04:36 +0530

    MErN read operations done

--- Node.JS_Course/PROJECTS/MErN_loginform/src/app.js
+++ Node.JS_Course/PROJECTS/MErN_loginform/src/app.js
@@ -19,10 +19,12 @@ app.set("view engine", 'hbs');
 app.set('views', template_path);
 hbs.registerPartials(partials_path);
 
+//index route
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+//register route
 app.get('/register', (req, res) => {
     res.render('register')
 });
@@ -42,9 +44,30 @@ app.post('/register', async (req, res) => {
                 confirmpassword: cpassword
             })
             const filled = await registerEmployee.save();
-            res.status(201).render(index);
+            res.status(201).render("index");
         } else {
             res.send("password don't match")
+        }
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+//login route
+app.get('/login', (req, res) => {
+    res.render('login')
+});
+
+app.post('/login', async (req, res) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        const userdata = await Register.findOne({ email: email }) //first one from databse:user enters || can simply be writter as ({email}) object destructuring
+        if (userdata.password === password) {
+            res.status(201).render("index");
+            // res.send(userdata)
+        } else {
+            res.send("Passwords don't match")
         }
     } catch (error) {
         res.status(400).send(error);
