f6fbd28330ce1e685fb6989fb00560ff4cb8d768
S4nfs <zyzzaestheticsgods@gmail.com>
2022-03-03 23:44:03 +0530

    JS updated with detailed hoisting & Scope chain explaination

--- JAVASCRIPT_course/1_Hoisting.js
+++ JAVASCRIPT_course/1_Hoisting.js
@@ -1,5 +1,4 @@
-//✔️ Hoisting in Javascript is a mechanism where variables and functions declarations are moved to the top of their scope before the code executes.
-
+//✔️ Hoisting in Javascript is a mechanism where variables and functions declarations are moved to the top of their scope before the code executes. (see image b)
 
 console.log(myName);
 var myName;
@@ -7,7 +6,29 @@ myName = "Sagar Verma";
 
 👆
 /*Steps:
-1. var myName;                 //Declaration or Define
-2. console.log(myName);        //Print
-3. myName = "Sagar Verma";     //Assignment or Initialization
+1. var myName = undefined;     
+2. console.log(myName);        //print undefined
+3. myName = "Sagar Verma";     //nothing executes
 */
+
+//Scope Chain - (see image c)
+var a ="Hello";
+
+function one(){
+    var b = "How are You?";
+    console.log(b);
+    two()
+
+    function two(){
+        var c = "What are you doing?"
+        console.log(a+b+c); //can access parent function + global context
+        three()
+    }
+}
+
+function three(){
+    var d = "Please answer, Bro?";
+    console.log(d);  //can only access its execution stack + global context (a,d)
+}
+
+one();
