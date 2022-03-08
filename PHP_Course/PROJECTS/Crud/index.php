0c608ac9a0099267faffb65d8a6d9a904f678c1e
S4nfs <zyzzaestheticsgods@gmail.com>
2022-03-08 22:18:39 +0530

    Full CRUD operation in pHP

--- PHP_Course/PROJECTS/Crud/index.php
+++ PHP_Course/PROJECTS/Crud/index.php
@@ -1,7 +1,7 @@
 <?php
     $server = "localhost";
     $username = "root";
-    $password = "";
+    $password = "root";
     $database = "notes";
  
 
--- PHP_Course/PROJECTS/FullCRUDoperation/connection.php
+++ PHP_Course/PROJECTS/FullCRUDoperation/connection.php
@@ -5,16 +5,16 @@ $username = "root";
 $db = "fullcrud";
 $password = "";
 
-$conn = mysqli_connect($server, $username,$db, $password);
+$conn = mysqli_connect($server, $username, $db, $password);
 
-if($conn){
-    ?>
+if ($conn) {
+?>
     <script>
         alert("connection Successfull");
     </script>
-    <?php
-}else{
-    die("Connection Error". mysqli_connect_error());
+<?php
+} else {
+    die("Connection Error" . mysqli_connect_error());
 }
 
 ?>
