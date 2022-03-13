d501d0bcebdc758cb59747bad6ae8f6d5751e1aa
S4nfs <zyzzaestheticsgods@gmail.com>
2022-03-13 13:58:48 +0530

    Path changed

---
+++ PHP_Course/OOPs/9_polymorphism.php
@@ -0,0 +1,21 @@
+<?php
+/*Polymorphism is the ability of an object to take on many forms.
+
+
+Pilymorphism
+    |
+    Compile Time
+        |
+        Function Overloading
+            |
+            Operator Overloading            //late binding
+
+    RunTime
+        |
+        Virtual Function                    //Early binding
+
+
+*/
+
+
+
