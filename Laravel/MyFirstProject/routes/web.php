f55053ee4299afa9e20a5c9e85482528eacccdb2
S4nfs <zyzzaestheticsgods@gmail.com>
2022-03-15 13:57:44 +0530

    forms submit

--- Laravel/MyFirstProject/routes/web.php
+++ Laravel/MyFirstProject/routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Usercontroller;
 use App\Http\Controllers\CustomController;
+use App\Http\Controllers\FormSubmit;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,14 +15,21 @@ use App\Http\Controllers\CustomController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 //3 methods to route:
-Route::get('/', function () {   //[1]using function
+Route::get('/', function () {                                       //[1]using function
     return view('welcome');
 });
-Route::get('users/{customer}', [Usercontroller::class, 'show']); //[2]from controller [must import above]
-// Route::view("user", "users"); //[3]from view
+Route::get('users/{customer}', [Usercontroller::class, 'show']);    //[2]from controller [must import above]
+// Route::view("url", "file");                                      //[3]from view
 
 
 Route::view('about', 'about');  //with header component
-Route::get('custom', [CustomController::class, 'loadview']);
+Route::get('custom', [CustomController::class, 'loadview']); //custom.blade
+Route::view('login', 'submit'); //form view
+Route::post('submit', [FormSubmit::class, 'getData']); //form post
 
+Route::view('noaccess', 'noaccess'); //middleware ageCheck
+Route::group(['middleware' => ['protectPage']],function(){ //middleware ageCheck group
+    Route::view('access', 'access');
+});
