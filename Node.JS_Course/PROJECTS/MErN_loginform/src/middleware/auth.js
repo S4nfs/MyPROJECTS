dd9aa5f640432aced42fcead18019f1789e62cc2
S4nfs <zyzzaestheticsgods@gmail.com>
2022-03-23 13:25:52 +0530

    error while restoring token from cookies

--- Node.JS_Course/PROJECTS/MErN_loginform/src/middleware/auth.js
+++ Node.JS_Course/PROJECTS/MErN_loginform/src/middleware/auth.js
@@ -7,9 +7,9 @@ const auth = async (req,res,next)=>{
     try{
         const cookie = req.cookies.jwt;
         const verifyUser = jwt.verify(cookie, process.env.SECRET_KEY);
-        console.log(verifyUser);
+        console.log(`Is user verified ${verifyUser}`);
         const user = await Register.findOne({_id:verifyUser._id});
-        console.log(user);
+        console.log(`Hellloooooooooo ${user.firstname}`);
         next();
     }catch(error){
         res.status(401).send(error);
