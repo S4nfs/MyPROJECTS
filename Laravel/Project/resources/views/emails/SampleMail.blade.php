6a730c6b973b21e60ebd6007cd70fcd9e9932cb2
S4nfs <zyzzaestheticsgods@gmail.com>
2022-03-26 19:22:29 +0530

    Route model Binding and Email Markdown

---
+++ Laravel/Project/resources/views/emails/SampleMail.blade.php
@@ -0,0 +1,16 @@
+@component('mail::message')
+# Introduction
+
+The body of your message.
+
+@component('mail::button', ['url' => ''])
+Button Text
+@endcomponent
+
+Thanks,<br>
+{{ config('app.name') }}
+@endcomponent
+
+@component('mail::panel')
+This is the panel content.
+@endcomponent
