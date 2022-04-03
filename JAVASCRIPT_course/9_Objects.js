d4866dfe39e303e449f858bb149c17202959e024
S4nfs <zyzzaestheticsgods@gmail.com>
2022-04-03 14:05:04 +0530

    ways to create new object

--- JAVASCRIPT_course/9_Objects.js
+++ JAVASCRIPT_course/9_Objects.js
@@ -2,17 +2,23 @@
 An object is a collection of properties and methods. 
 Object = School bag
 
+There are different ways to create new objects:
+1. Create a single object, using an object literal ex.below examples.
+2. Create a single object, with the keyword new.
+3. Define an object constructor, and then create objects of the constructed type.
+4. Create an object using Object.create().
+
 
 There are two ways to access objects properties using:
 1. Dot Notation     - you can see below bioData.myname to access value "Sagar Verma"
-2. Bracket Notation - you can also use bioData["myname"] to access value "Sagar Verma" (useful if you have a key with spaces)
+2. Bracket Notation - you can also use bioData["myname"] to access value "Sagar Verma" (useful if you have a key with spaces OR with arrays)
 
 */
 
 
 //✔️How to create an object (that include both variables and function)?
 // let bioData = {
-//  // Key    : "Value",
+// //Key/property : "Value",
 //     myname : "Sagar",
 //     myage  : 30,
 //     getData (){                      //if a function is call inside an object we call it now "method" usually
@@ -56,11 +62,27 @@ Note:(this) object will not work wth fat arrow function
 //    myName();                                                        //run on browser
 
 
-
 //✔️ Objects to arrays & array to objects conversion
-const person = {name : 'Sagar', age : 30 };
-console.log(Object.values(person));                                    
-//vice-versa
-const arrObj = Object.entries(person);
-console.log(Object.fromEntries(arrObj));
+// const person = {name : 'Sagar', age : 30 };
+// console.log(Object.values(person));                                    
+// //vice-versa
+// const arrObj = Object.entries(person);
+// console.log(Object.fromEntries(arrObj));
    
+
+//✔️ Check property value using hasOwnProperty method
+// var obj = {
+//     "animal": "cat",
+//     "tree": "Eucalyptus",
+//     "Language": "Japanese"
+// };
+// function checkmyObject(val){
+//     if(obj.hasOwnProperty(val)){
+//         return obj[val];
+//     }else{
+//         return "Not Found";
+//     }
+// }
+// console.log(checkmyObject("tree"));
+
+
