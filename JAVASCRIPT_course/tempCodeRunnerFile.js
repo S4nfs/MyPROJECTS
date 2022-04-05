83be3062ce6b20954e0763541f64543132ca795a
S4nfs <zyzzaestheticsgods@gmail.com>
2022-04-05 23:23:18 +0530

    del

--- JAVASCRIPT_course/tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-const person = {name : 'Sagar', age : 30 };
-// console.log(Object.values(person));                                    
-// //vice-versa
-// const arrObj = Object.entries(person);
-// console.log(Object.fromEntries(arrObj));
