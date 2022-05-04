3179dcd4f6a506b695e71262ceb6a015af7ba2e4
S4nfs <zyzzaestheticsgods@gmail.com>
2022-05-04 12:51:04 +0530

    register page with placeholders and php added

--- PHP_Course/PROJECTS/DatabseWithOOps/index2.php
+++ PHP_Course/PROJECTS/DatabseWithOOps/index2.php
@@ -8,16 +8,34 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="insert.php" method="post">
-    <input type="text" name="name" id="">
-    <input type="text" name="degree" id="">
-    <input type="email" name="email" id="">
-    <input type="text" name="mobile" id="">
-    <input type="text" name="refer" id="">
-    <input type="text" name="jobpost" id="">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    <input type="text" name="name" id="" placeholder="Name">
+    <input type="text" name="degree" id="" placeholder="Degree">
+    <input type="email" name="email" id="" placeholder="Email">
+    <input type="text" name="mobile" id="" placeholder="Mobile">
+    <input type="text" name="refer" id="" placeholder="Refer">
+    <input type="text" name="jobpost" id="" placeholder="Jobpost">
 
     <button type="submit">Create User</button>
 </form>
+
+<?php
+include 'autoloader.php';
+
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    $name = $_POST['name'];
+    $degree = $_POST['degree'];
+    $email = $_POST['email'];
+    $mobile = $_POST['mobile'];
+    $refer = $_POST['refer'];
+    $jobpost = $_POST['jobpost'];
+
+$demo = new SignupController($name, $degree, $email, $mobile, $refer, $jobpost);
+echo $demo->signupUser();
+header("location: index2.php?error=none");
+}
+
+?>
 </body>
 </html>
 
