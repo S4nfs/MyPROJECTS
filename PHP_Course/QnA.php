f1c8d8082da79f4c05fee1d097a7466d60cb0eb7
S4nfs <zyzzaestheticsgods@gmail.com>
2022-05-14 12:37:28 +0530

    formatted

--- PHP_Course/QnA.php
+++ PHP_Course/QnA.php
@@ -1,9 +1,13 @@
 <?php
-/* Q1   *
+/*
+====================================================================================================================================
+ Q1   *
         **
         ***
         ****
-        *****   ❔ */
+        *****   ❔ 
+====================================================================================================================================
+*/
 
 // for ($i = 1; $i <= 5; $i++) {
 //     for ($j = 1; $j <= $i; $j++) {
@@ -12,7 +16,11 @@
 //     echo "</br>";
 // }
 
-/* Q2.Check whether an iput is palindrome or not ex. "level" is palindrome ❔ */
+/*
+====================================================================================================================================
+ Q2.Check whether an input is palindrome or not ex. "level" is palindrome ❔
+====================================================================================================================================
+*/
 
 // function isPalin($str){
 //     if(strrev($str) == $str){
@@ -22,13 +30,6 @@
 // }
 // }
 // echo isPalin("level");
-
-/* Q3. ❔
-
-Input: nums = [0,0,1,1,1,2,2,3,3,4]
-Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
-*/
-
 
 // function removeDuplicates( &$nums ) {
 //    $unique = array_unique($nums);
@@ -43,6 +44,13 @@ Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
 // echo $k;
 
 
+/*
+====================================================================================================================================
+ Q3. ❔
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+====================================================================================================================================
+*/
  function removeDuplicates($nums) {
         // Length of the updated array
         $count = 0;
@@ -60,7 +68,11 @@ Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
     };
     echo removeDuplicates([0,0,1,1,1,2,2,3,3,4]);
 
-// Wrte function that return the average of 3 highest grades as an integer. The function takes the 5 subjects grades as parameters ❔
+/*
+====================================================================================================================================
+Q3. Wrte function that return the average of 3 highest grades as an integer. The function takes the 5 subjects grades as parameters ❔
+====================================================================================================================================
+*/
 
 function best_three($sub1, $sub2, $sub3, $sub4, $sub5){
     //first check grades if grades are below 0 return -1 and if grades are above 100 return -2
