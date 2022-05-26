246f7e9a3eb1f28f5266638836163d46514788f5
S4nfs <zyzzaestheticsgods@gmail.com>
2022-05-26 21:39:49 +0530

    handling return promises

--- Node.JS_Course/PROJECTS/Task_ManagerApp/controllers/task.js
+++ Node.JS_Course/PROJECTS/Task_ManagerApp/controllers/task.js
@@ -1,23 +1,41 @@
 const Task = require('../models/task');
-
-const getAllTasks = (req, res) => {
-    res.send('  all items');
-}
-const createTask = async (req, res) => {
+//Get all
+const getAllTasks = async (req, res) => {
     try {
-        const task = await Task.create(req.body)
-        res.status(201).json({ task })
+        const alltask = await Task.find({})
+        res.status(200).json({ alltask })
     } catch (error) {
         res.status(500).json({ msg: error })
     }
 
 }
-const getTask = (req, res) => {
-    res.json({ id: req.params.id });
+//Create
+const createTask = async (req, res) => {
+    try {
+        const ctask = await Task.create(req.body)
+        res.status(201).json({ ctask })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+
 }
+//Get by id
+const getTask = async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id)
+        if (!task) {
+            return res.status(404).json({ msg: `NO task with id : ${taskID}` })
+        }
+        res.status(200).json({ getTask })
+    } catch (error) {
+        res.status(500).json({ msg: error })
+    }
+}
+//Update
 const updateTask = (req, res) => {
     res.send('update items');
 }
+//Delete
 const deleteTask = (req, res) => {
     res.send('delete items');
 }
