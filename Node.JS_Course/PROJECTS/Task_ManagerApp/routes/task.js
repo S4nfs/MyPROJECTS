ece3164cf22026abffc287c8805014c526f5fd4e
S4nfs <zyzzaestheticsgods@gmail.com>
2022-05-27 20:06:50 +0530

    route operations

--- Node.JS_Course/PROJECTS/Task_ManagerApp/routes/task.js
+++ Node.JS_Course/PROJECTS/Task_ManagerApp/routes/task.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const{getAllTasks, createTask, getTask, updateTask, deleteTask} = require('../controllers/task');
+const { getAllTasks, createTask, getTask, updateTask, deleteTask } = require('../controllers/task');
 
 router.route('/').get(getAllTasks).post(createTask);
 router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
