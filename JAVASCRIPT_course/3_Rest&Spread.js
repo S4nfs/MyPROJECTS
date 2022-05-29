ec3b1201586879d0b58172af53901da557f2d181
S4nfs <zyzzaestheticsgods@gmail.com>
2022-05-29 12:08:58 +0530

    added apply method

--- JAVASCRIPT_course/3_Rest&Spread.js
+++ JAVASCRIPT_course/3_Rest&Spread.js
@@ -2,17 +2,19 @@
 
 
 //Rest in ES6---------------------------------------------------
-function addnumbers(a,b,...other){
+function addnumbers(a, b, ...other) {
     // console.log(other[0]); //6
-    return a+b;
+    return a + b;
 }
-const res = addnumbers(1,2,6,3);
+const res = addnumbers(1, 2, 6, 3);
 console.log(res);
 
 //Spread---------------------------------------------------------
-var names = ["Ajay","Sagar","Prateek"];
-function getNames(name1,name2,name3){
-    console.log(name1,name2,name3);
+function getNames(name1, name2, name3) {
+    console.log(name1, name2, name3);
 }
-getNames(names[0],names[1],names[2]);       //Ajay Sagar Prateek
+var names = ["Ajay", "Sagar", "Prateek"];
+getNames(names[0], names[1], names[2]);     //Ajay Sagar Prateek
 getNames(...names)                          //Ajay Sagar Prateek
+getNames.apply(null,names)                  //apply(this, arr) is same as call() but it is used to pass array as argument
+
