3f600b2988c41232bdee8bed3b3b3db9c83cbf39
S4nfs <zyzzaestheticsgods@gmail.com>
2022-06-06 23:15:30 +0530

    verifying JWT with header bearer token

---
+++ Node.JS_Course/PROJECTS/JWT_Basics/middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const CustomAPIError = require('../errors/custom-error')
+
+const authenticationMiddleware = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        throw new CustomAPIError('No token ', 401);
+    }
+    const token = authHeader.split(' ')[1];
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const {id, username} = decoded;
+        req.user = {id,username};
+        next();
+    } catch (error) {
+        throw new CustomAPIError('Not authorized to access this route ', 401);
+    }
+}
+
+module.exports = authenticationMiddleware;
