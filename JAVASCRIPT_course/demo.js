da644cf4fc8049c56c3fac0312b32ff0b879fcf4
S4nfs <zyzzaestheticsgods@gmail.com>
2022-06-07 21:02:14 +0530

    debugging purpose

--- JAVASCRIPT_course/demo.js
+++ JAVASCRIPT_course/demo.js
@@ -1,7 +1,9 @@
-function multiply(arr, n) {
-    if (n <= 0) {
-      return 1;
-    } else {
-      return multiply(arr, n - 1) * arr[n - 1];
-    }
-  }
+let arr = [2, 3, 4, 6, 8];
+let outarr = arr.map((outpitelement) => {                               //👉 Can be written In Single line: 
+    return outpitelement * 2;                            
+}).filter((finalelement) => {
+    return finalelement > 10;
+})
+// 👇Single line: (using => instead of return)
+// let outarr = arr.map((outpitelement) => outpitelement * 2).filter((finalelement) => finalelement > 10);
+console.log(outarr);
