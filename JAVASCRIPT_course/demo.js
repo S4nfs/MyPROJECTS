05bf7f3e5f8748c0efed7232a59f0d45749e6cd3
S4nfs <zyzzaestheticsgods@gmail.com>
2022-06-11 00:04:40 +0530

    debug purpose

--- JAVASCRIPT_course/demo.js
+++ JAVASCRIPT_course/demo.js
@@ -1,9 +1,5 @@
-let arr = [2, 3, 4, 6, 8];
-let outarr = arr.map((outpitelement) => {                               //👉 Can be written In Single line: 
-    return outpitelement * 2;                            
-}).filter((finalelement) => {
-    return finalelement > 10;
-})
-// 👇Single line: (using => instead of return)
-// let outarr = arr.map((outpitelement) => outpitelement * 2).filter((finalelement) => finalelement > 10);
-console.log(outarr);
+function findLongestWordLength(str) {
+  return Math.max(...str.split(" ").map(word => word.length));
+}
+
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
