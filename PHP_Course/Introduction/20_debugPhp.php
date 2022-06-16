93bc6ec96588c50c570e5271ce13991f4bf69204
S4nfs <zyzzaestheticsgods@gmail.com>
2022-06-16 21:15:14 +0530

    php error handling server side

---
+++ PHP_Course/Introduction/20_debugPhp.php
@@ -0,0 +1,7 @@
+<?php
+//1. error handling in php file:
+error_reporting(E_ALL | E_STRICT);  
+ini_set('display_startup_errors',1);  
+ini_set('display_errors',1);
+
+//2. Edit your php.ini file and set the display_errors and display_startup_errors options to On so that you can have a chance to see what the problem is.
