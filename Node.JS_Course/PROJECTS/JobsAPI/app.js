b44eb365827e11649ba67e184b34867dd126eaac
S4nfs <zyzzaestheticsgods@gmail.com>
2022-06-20 22:37:48 +0530

    rouets added

--- Node.JS_Course/PROJECTS/JobsAPI/app.js
+++ Node.JS_Course/PROJECTS/JobsAPI/app.js
@@ -2,10 +2,18 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+
+//connect DB
+const connectDB = require('./db/connect')
+//routes
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+const { connect } = require('mongoose');
 
 app.use(express.json());
 // extra packages
@@ -15,13 +23,15 @@ app.get('/', (req, res) => {
   res.send('jobs api');
 });
 
+app.use('api/v1/auth', authRouter);
+app.use('api/v1/jobs', jobsRouter);
+
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
-
 const start = async () => {
   try {
+    await connect(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
