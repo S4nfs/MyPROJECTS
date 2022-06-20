db160e9498ac2e12346f2e03038d04784bb8ba11
S4nfs <zyzzaestheticsgods@gmail.com>
2022-06-20 22:37:11 +0530

    connection to mongoDB

--- Node.JS_Course/PROJECTS/JobsAPI/db/connect.js
+++ Node.JS_Course/PROJECTS/JobsAPI/db/connect.js
@@ -3,9 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = (url) => {
   return mongoose.connect(url, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   })
 }
 
