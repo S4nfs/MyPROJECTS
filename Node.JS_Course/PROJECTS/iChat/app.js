87754da7d6d965c2ed8f83450d46c4639cb6edbd
S4nfs <zyzzaestheticsgods@gmail.com>
2022-06-24 23:12:43 +0530

    socket.io server

--- Node.JS_Course/PROJECTS/iChat/app.js
+++ Node.JS_Course/PROJECTS/iChat/app.js
@@ -3,8 +3,25 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const server = app.listen(port, () => {
-    console.log(`Listening iChat on port ${port}`)
-})
+const server = app.listen(port, () => console.log(`Listening iChat on port ${port}`))
+
+let socketsConnected = new Set()
+const io = require('socket.io')(server)
+io.on('connection', onConnected)
+
+function onConnected(socket) {
+    console.log(socket.id);
+    socketsConnected.add(socket.id)
+
+    //to emit an event to client side when the socket is connected
+    io.emit('clients-total', socketsConnected.size)
+
+    socket.on('disconnect', () => {
+        console.log('socket disconnected', socket.id);
+        socketsConnected.delete(socket.id)
+        io.emit('clients-total', socketsConnected.size)
+
+    })
+}
