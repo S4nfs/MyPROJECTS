2416aa76ce3f612e07ef8d56e531229783db3869
S4nfs <zyzzaestheticsgods@gmail.com>
2022-06-25 20:54:26 +0530

    feedback data emit to client side after receiving

--- Node.JS_Course/PROJECTS/iChat/app.js
+++ Node.JS_Course/PROJECTS/iChat/app.js
@@ -7,7 +7,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const server = app.listen(port, () => console.log(`Listening iChat on port ${port}`))
 
-let socketsConnected = new Set()
+let socketsConnected = new Set();
+
 const io = require('socket.io')(server)
 io.on('connection', onConnected)
 
@@ -22,6 +23,15 @@ function onConnected(socket) {
         console.log('socket disconnected', socket.id);
         socketsConnected.delete(socket.id)
         io.emit('clients-total', socketsConnected.size)
+    });
 
+    //handling hellomessage event from client side
+    socket.on('sendMessageToAll', (data) =>{
+        socket.broadcast.emit('toALLThePeopleConnectedInTheLobby', data) //If you want to broadcast to everyone except the person who connected you can use socket.broadcast.emit()
+    })
+
+
+    socket.on('someoneIsTyping', (data) => {
+        socket.broadcast.emit('feedback', data)
     })
 }
