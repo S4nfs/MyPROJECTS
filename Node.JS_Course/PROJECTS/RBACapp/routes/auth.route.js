4f0c4f15f7bfc9637e6f089471e5957e48fc398e
S4nfs <zyzzaestheticsgods@gmail.com>
2022-07-01 23:46:08 +0530

    rendering auth routes

--- Node.JS_Course/PROJECTS/RBACapp/routes/auth.route.js
+++ Node.JS_Course/PROJECTS/RBACapp/routes/auth.route.js
@@ -1,22 +1,29 @@
 const router = require('express').Router();
+const User = require('../models/user.model');
 
-router.get('/login', (req, res, next) => {
+router.get('/login', async (req, res, next) => {
     res.render("login");
 })
 
-router.get('/register', (req, res, next) => {
+router.get('/register', async (req, res, next) => {
     res.render("register");
 })
 
-router.post('/login', (req, res, next) => {
-    res.send("login");
+router.post('/login', async (req, res, next) => {
+    res.send(req.body);
 })
 
-router.post('/register', (req, res, next) => {
-    res.send("register post");
+router.post('/register', async (req, res, next) => {
+    try{
+    const user = new User(req.body);
+    await user.save()
+    res.send(user);
+    }catch(error){
+        next(error)
+    }
 })
 
-router.post('/logout', (req, res, next) => {
+router.post('/logout', async (req, res, next) => {
     res.send("logout post");
 })
 
