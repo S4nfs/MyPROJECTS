08ab453f3a7f17709199cad4132d4f088d366bb8
S4nfs <zyzzaestheticsgods@gmail.com>
2022-07-02 22:42:49 +0530

    hashing password with isNew mongoose property to check if the document is new

--- Node.JS_Course/PROJECTS/RBACapp/models/user.model.js
+++ Node.JS_Course/PROJECTS/RBACapp/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const UserSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -11,5 +12,16 @@ const UserSchema = new mongoose.Schema({
         required: true
     },
 })
-
+UserSchema.pre('save', async function (next) { //cant use arrow function here (becoz of ||this||)
+    try {
+        if (this.isNew) { //isNew is a property in mongoose which tells if the document is new or not because evertime we hit the user.save() method it will hasshed the password again
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(this.password, salt)
+            this.password = hashedPassword; //overwriting the password with hased password
+        }
+        next();
+    } catch (err) { 
+        next(err)
+    }
+})
 module.exports = mongoose.model('user', UserSchema);
