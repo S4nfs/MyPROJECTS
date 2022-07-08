589fa188566a5d342a8950bbf8d02189ba237611
S4nfs <zyzzaestheticsgods@gmail.com>
2022-07-08 12:00:06 +0530

    get user object from passport

--- Node.JS_Course/PROJECTS/RBACapp/routes/user.route.js
+++ Node.JS_Course/PROJECTS/RBACapp/routes/user.route.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
 router.get('/profile', async(req,res,next) => {
-    res.render('profile');
+    // console.log(req.user)
+    const person = req.user
+    res.render('profile', {person});
 })
 module.exports = router;
