fd638713587bbe9a778fff728ed622d5f8e0d852
S4nfs <zyzzaestheticsgods@gmail.com>
2022-08-16 23:17:09 +0530

    anagram

--- JAVASCRIPT_course/QnA.js
+++ JAVASCRIPT_course/QnA.js
@@ -1,8 +1,9 @@
+//==============================================================================================================================
 /* Q1: Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 Input: nums = [2,5,5,11], target = 10
 Output: [1,2]
 */
-
+//==============================================================================================================================
 //Solution:
 var twoSum = function (nums, target) {
   for (var i = 0; i < nums.length; i++) {
@@ -14,23 +15,26 @@ var twoSum = function (nums, target) {
 };
 console.log(twoSum([2, 5, 5, 11], 10));
 
-
+//==============================================================================================================================
 /* Q2   *
         **
         ***
         ****
         *****   */
-
+//==============================================================================================================================
 
 for (var i = "*"; i.length < 5; i += "*") {
   console.log(i);
 }
 
+//==============================================================================================================================
 /* Q3.Check whether an iput is palindrome or not ex. "level" is palindrome */
+//==============================================================================================================================
 
 
-
+//==============================================================================================================================
 /* Q4. Reverse a string */
+//==============================================================================================================================
 function reverseString(str) {
   let reversedStr = "";
   for (let i = str.length - 1; i >= 0; i--) {
@@ -40,7 +44,9 @@ function reverseString(str) {
 }
 console.log(reverseString("hello"));
 
+//==============================================================================================================================
 /* Q5. Return Largest Numbers in Arrays */
+//==============================================================================================================================
 function largestOfFour(arr) {
   let result = [];
   for (let i = 0; i < arr.length; i++) {
@@ -68,16 +74,17 @@ function largestOfFour(arr) {
 console.log(largestOfFour([[17, 23, 25, 12], [25, 7, 34, 48], [4, 10, 18, 21], [72, 3, 17, 10], [2, 33, 4, 8]]));
 
 
-
+//==============================================================================================================================
 /* Q6 Confirm the Ending */
-
+//==============================================================================================================================
 function confirmEnding(str, target) {
   return str.slice(str.length - target.length) === target; //7 - 4 = 3 which starts from 0
 }
 console.log(confirmEnding("newname", "name"));
 
+//==============================================================================================================================
 /* Q7 Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.*/
-
+//==============================================================================================================================
 function titleCase(str) {
   let splitter = str.split(" ");
   let newarr = [];
@@ -89,7 +96,9 @@ function titleCase(str) {
 
 console.log(titleCase("I'm a little tea pot"));
 
+//==============================================================================================================================
 /* Q8 Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array. */
+//==============================================================================================================================
 
 function mutation(arr) {
   let test = arr[1].toLowerCase();
@@ -101,11 +110,12 @@ function mutation(arr) {
 }
 
 console.log(mutation(["hello", "hey"]));
-
+//==============================================================================================================================
 /* Q9 Checking Sun Zero
 Input: nums = [-5, -4, -3, -2, 0, 2,4,6,8]
 Output: [-4, 4] sum should be 0
 */
+//==============================================================================================================================
 
 //o(n^2) quadratic time complexity
 function getSumZero(array) {
@@ -131,10 +141,36 @@ function findSomePair(array) {
 
     } else if (sum > 0) {
       right--;
-    } else{
-      left ++;
+    } else {
+      left++;
     }
   }
 }
 
 console.log(findSomePair([-5, -4, -3, -2, 0, 2, 4, 6, 8]));
+
+//==============================================================================================================================
+/* Q.10 String anagram
+'hello' -> 'llheo' no an anagram
+'sagar' -> 'agars'  is anagram
+*/
+//==============================================================================================================================
+
+function isAnagram(string1, string2) {
+  if (string1.length !== string2.length) {
+    return false;
+  }
+  let counter = {};
+  for (let letter of string1) {
+    counter[letter] = (counter[letter] || 0) + 1;
+    // console.log(counter[letter]);
+  }
+  for(let items of string2){
+    if(!counter[items]){
+      return false;
+    }
+    counter[items] -= 1;
+  }
+  return true;
+}
+console.log(isAnagram('hello', 'llheo'));
