c0f8fb3b0495a2e3e52b9ecf8e270fb360275815
S4nfs <zyzzaestheticsgods@gmail.com>
2022-08-26 21:17:16 +0530

    dsa question series updated

--- JAVASCRIPT_course/QnA.js
+++ JAVASCRIPT_course/QnA.js
@@ -329,3 +329,27 @@ function sortArray(array) {
 }
 sortArray(myarray);
 console.log(newarray);
+
+
+//==============================================================================================================================
+/* Q.16 Helper Recursive Function
+Input: [1,2,3,4,5,6,7,8,9,10]
+Output: [1,3,5,7,9]  //odd numbers
+*/
+//==============================================================================================================================
+function findOdd(array){
+  let result = [];
+  function helperFunction(inputArray){    
+    if(inputArray.length === 0){
+      return;       //to exit inner function
+    }
+    if(inputArray[0]%2 !== 0){
+      result.push(inputArray[0])
+    }
+    helperFunction(inputArray.slice(1))
+  }
+  helperFunction(array)
+  return result;
+}
+const res = findOdd([1,2,3,4,5,6,7,8,9,10]);
+console.log(res);
