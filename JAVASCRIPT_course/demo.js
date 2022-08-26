9716aff72423b0d315c08d9e2ec31e2dd6f29a98
S4nfs <zyzzaestheticsgods@gmail.com>
2022-08-26 12:21:15 +0530

    debugging recursion

--- JAVASCRIPT_course/demo.js
+++ JAVASCRIPT_course/demo.js
@@ -1,22 +1,12 @@
-const fs = require('fs');
-const path = ['./demo2.js', './QnA.js'];
-var logs = [];
-fs.access(path[0], (error) => {
-  if (error) {
-      console.log(error);
-      return;
-    }
-      fs.readFile(path[0], (err, data) => {
-          if (err) {
-              console.log(err);
-          } else {
-              console.log(`yes`);
-              var array = data.toString().split("\n");
-              logs.push(array);
-          }
-      }
-      );
-}
-);
 
-console.log(logs);
+let i = 0, j = 1, myarray= [2,3,1,4];
+function isSorted(array) {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > array[i + 1]) {
+        return false
+      }
+      return true;
+    }
+  }
+
+  console.log(isSorted(myarray))
--- JAVASCRIPT_course/demo2.js
+++ JAVASCRIPT_course/demo2.js
@@ -1,10 +1,31 @@
-function mutation(arr) {
-  let test = arr[1].toLowerCase();
-  let target = arr[0].toLowerCase();
-  for (let i = 0; i < test.length; i++) {
-    if (target.indexOf(test[i]) < 0) return false; //return -1 if not found otherwise return index 
+
+let i = 0, j = 1, myarray= [2,3,1,4], newarray = [];
+
+function isSorted(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > array[i + 1]) {
+      return false
+    }
   }
   return true;
 }
 
-console.log(mutation(["hello", "hey"]));
+
+function sortArray(array) {
+  if (isSorted(array)) {
+    // newarray = array;
+    console.log(array)
+    return array; 
+  } else if (array[i] < array[j]) {
+    i++;
+    j++;
+    sortArray(array)  //recursion as the fuction call itself
+  } else {
+    [array[i],array[j]] = [array[j],array[i]]
+    i = 0;
+    j = 1;
+    sortArray(array)  //recursion
+  }
+}
+const res = sortArray(myarray);
+console.log(res);
