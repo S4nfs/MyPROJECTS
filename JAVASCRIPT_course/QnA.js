180fd47dd2606bf42bd33e5e2f6a6609a572db8c
S4nfs <zyzzaestheticsgods@gmail.com>
2022-09-01 23:34:04 +0530

    insertion sort algorithm

--- JAVASCRIPT_course/QnA.js
+++ JAVASCRIPT_course/QnA.js
@@ -430,6 +430,21 @@ function SelectionSort(array) {
 console.log(SelectionSort([0, 2, 34, 22, 10, 19]))
 
 //==============================================================================================================================
-/* Q.20 Bubble Sort - a sorting algorith where the largest values bubble up at the top [left -> right]
+/* Q.20 Insertion Sort -
 */
 //==============================================================================================================================
+const insertionSort = (array) => {
+
+  for (i = 1; i < array.length; i++) {
+    let curr = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > curr) {
+      array[j + 1] = array[j];  //temp [8,8,4,1,3]
+      j--;
+    }
+    array[j + 1] = curr;  //-1+1 = 0 [2,8,4,1,3]
+  }
+  return array;
+}
+
+console.log(insertionSort([8, 2, 4, 1, 3]));
