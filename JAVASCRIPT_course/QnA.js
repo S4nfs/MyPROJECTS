b5ab5d40363627f6060aec23453dc784d8e25c6c
S4nfs <zyzzaestheticsgods@gmail.com>
2022-09-02 23:46:08 +0530

    DSA series updated

--- JAVASCRIPT_course/QnA.js
+++ JAVASCRIPT_course/QnA.js
@@ -448,3 +448,27 @@ const insertionSort = (array) => {
 }
 
 console.log(insertionSort([8, 2, 4, 1, 3]));
+
+//==============================================================================================================================
+/* Q.20 Find the maximum occurence characters in a string
+*/
+//==============================================================================================================================
+
+
+const maxCharacters = (str) => {
+  const alpha = {};
+  str.split("").forEach(element => {
+    alpha[element] = alpha[element] ? alpha[element] + 1 : 1;   //key:value
+  });
+
+  let max = 1;
+  let letter
+  for (let k in alpha) {  //get value
+    if (alpha[k] > max) {
+      max = alpha[k];
+      letter = k;
+    }
+  }
+  return letter;
+}
+console.log(maxCharacters("Hello World"));
