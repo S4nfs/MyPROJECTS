2b53f82c5b779842a5a45994c3e6b77fae08c4da
S4nfs <zyzzaestheticsgods@gmail.com>
2022-09-03 21:04:01 +0530

    project directory changed

--- JAVASCRIPT_course/demo.js
+++ JAVASCRIPT_course/demo.js
@@ -3,16 +3,8 @@ const maxCharacters = (str) => {
   const alpha = {};
   str.split("").forEach(element => {
     alpha[element] = alpha[element] ? alpha[element] + 1 : 1;   //key:value
-  });
 
-  let max = 1;
-  let letter
-  for (let k in alpha) {  //get value
-    if (alpha[k] > max) {
-      max = alpha[k];
-      letter = k;
-    }
-  }
-  return letter;
+    console.log(alpha)
+  })
 }
 console.log(maxCharacters("Hello World"));
