5bb3f5fcfb754ca9a02c1526bca0c07c78188324
S4nfs <zyzzaestheticsgods@gmail.com>
2022-09-05 13:40:17 +0530

    Longest Substring without repeating characters

--- JAVASCRIPT_course/QnA.js
+++ JAVASCRIPT_course/QnA.js
@@ -513,6 +513,33 @@ console.log(maxCharacters("Hello World"));
 
 //==============================================================================================================================
 /* Q.21 Longest Substring without repeating characters
+Input: str = "pwwkew"
+Output: 3
 */
 //==============================================================================================================================
 
+const lengthOfLongestSubstring = (str) => {
+
+  //Sliding window
+  if (!str) {
+    return 0;
+  }
+  //pointers
+  let end = 0;
+  let start = 0;
+  let maxLength = 0;
+
+  const unique = new Set();
+
+  while (end < str.length) {
+    if (unique.has(str[end])) {
+      unique.delete(str[start++])
+    } else {
+      unique.add(str[end++]);
+      maxLength = Math.max(maxLength, unique.size);
+    }
+  }
+  return maxLength;
+}
+
+console.log(lengthOfLongestSubstring("pwwkew"));
