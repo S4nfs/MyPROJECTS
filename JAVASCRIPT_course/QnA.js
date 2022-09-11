fc10c5475e290f19f34e23d5ed68b7ec129e616f
S4nfs <zyzzaestheticsgods@gmail.com>
2022-09-11 23:16:43 +0530

    valid parentheses

--- JAVASCRIPT_course/QnA.js
+++ JAVASCRIPT_course/QnA.js
@@ -582,3 +582,26 @@ Output: false
 
 */
 //==============================================================================================================================
+
+const isValid = (s) => {
+  const stack = [];
+
+  for (let i = 0; i < s.length; i += 1) {
+    const top = stack[stack.length - 1];
+    if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+      stack.push(s[i]);
+    } else if (s[i] === ')' && top === '(' && stack.length !== 0) {
+      stack.pop();
+    } else if (s[i] === ']' && top === '[' && stack.length !== 0) {
+      stack.pop();
+    } else if (s[i] === '}' && top === '{' && stack.length !== 0) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
+};
+
+console.log(isValid("()[]{}"))
