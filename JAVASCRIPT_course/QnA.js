cfe53389aec022a4d671c7fb932266e066acfe1d
S4nfs <zyzzaestheticsgods@gmail.com>
2022-09-21 23:04:14 +0530

    DSA updates

--- JAVASCRIPT_course/QnA.js
+++ JAVASCRIPT_course/QnA.js
@@ -607,3 +607,18 @@ const isValid = (s) => {
 };
 
 console.log(isValid("()[]{}"))
+
+//==============================================================================================================================
+/* Q.24 FizzBuzz Challenge
+Create a for loop that iterates up to 100 while outputting "fizz" at multiples of 3, "buzz" at multiples of 5 and "fizzbuzz" at multiples of 3 and 5.
+*/
+//==============================================================================================================================
+
+const appleFizz = () => {
+  for (i = 1; i <= 100; i++) {
+    let f = i % 3 == 0,
+      b = i % 5 == 0;
+    console.log(f ? (b ? 'FizzBuzz' : 'Fizz') : b ? 'Buzz' : i);
+  }
+}
+appleFizz();
