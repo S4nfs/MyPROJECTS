2ad50b00bdee2225618b0345a763c312348dc252
S4nfs <zyzzaestheticsgods@gmail.com>
2022-09-25 22:06:58 +0530

    grep command added

--- Linux_Course/shebang/bash.sh
+++ Linux_Course/shebang/bash.sh
@@ -249,6 +249,27 @@ done
 
 #Putting this single pipe | is also known as pipe in Bash. It is used when the output of the first command works as an input of the second command.
 
+#🐹 grep -----------------------------------------------------------------------------------------------------------------------------
+# The grep filter searches a file for a particular pattern of characters, and displays all lines that contain that pattern
+
+grep [options] pattern [files]
+# -c : This prints only a count of the lines that match a pattern
+# -h : Display the matched lines, but do not display the filenames.
+# -i : Ignores, case for matching
+# -l : Displays list of a filenames only.
+# -n : Display the matched lines and their line numbers.
+# -v : This prints out all the lines that do not matches the pattern
+# -e exp : Specifies expression with this option. Can use multiple times.
+# -f file : Takes patterns from file, one per line.
+# -E : Treats pattern as an extended regular expression (ERE)
+# -w : Match whole word
+# -o : Print only the matched parts of a matching line,
+#  with each such part on a separate output line.
+
+# -A n : Prints searched line and nlines after the result.
+# -B n : Prints searched line and n line before the result.
+# -C n : Prints searched line and n lines after before the result.
+
 #🐹🐹🐹🐹🐹🐹🐹🐹🐹🐹🐹🐹🐹🐹🐹------------------Some Bash Script Examples------------------🐹🐹🐹🐹🐹🐹🐹🐹🐹🐹🐹🐹🐹🐹🐹
 
 #Below it's an approach to detect Debian and RedHat based Linux OS making use of the /etc/lsb-release and /etc/os-release (depending on the Linux flavor you're using) and take a simple action based on it.
