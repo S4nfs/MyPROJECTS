da368b93074e5c64d1d081351223a0d183b039ba
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-04 13:25:28 +0530

    adding functionalities using higher-order component (HOC)

---
+++ REACT-JS_Course/hello-react/src/components/PersonB.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import subjects from './HOC'
+
+export class PersonB extends Component {
+    state = {
+        marks: 0
+    }
+
+    marksIncrease = () => {
+        this.setState({ marks: this.state.marks + 1 })
+    }
+    render() {
+        return (
+            <div>
+                <div></div>
+                <h3 onMouseOver={this.marksIncrease}>Person B marks is {this.state.marks} in {this.props.hocsub}</h3>
+            </div>
+        )
+    }
+}
+
+export default subjects(PersonB)
