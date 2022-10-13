e09e179a4af4f038932bd55090ef6f04eab6cca1
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-13 23:53:52 +0530

    debug

--- JAVASCRIPT_course/demo2.js
+++ JAVASCRIPT_course/demo2.js
@@ -1,51 +1,5 @@
-// const Sort = (array) => {
-//   for (let i = 1; i < array.length; i++) {
-//     let curr = array[i];
-//     let j;
-//     for (j = i - 1; j >= 0; j--) {
-//       if (array[j] >= 0 && array[j] > curr) {
-//         array[j + 1] = array[j];
-//       }
-//     }
-//     array[j + 1] = curr;
-//   }
-//   return array
-// }
-
-// console.log(Sort([8, 2, 4, 1, 3]))
-
-
-class Stack {
-
-    constructor() {
-        this.elements = [];
-    }
-    push(element) {
-        this.elements.push(element);
-    }
-    pop() {
-        if (this.elements.length === 0) return "Underflow Situation"
-        else return this.elements.pop();
-    }
-    isEmpty() {
-        if (this.elements.length > 0) return false;
-        else return true;
-    }
-    reverse(str) {
-
-        let i = 0, result = "";
-        while (i !== str.length) {
-            this.push(str.charAt(i));
-            i++
-        }
-        while (!this.isEmpty()) {
-            result += this.pop()
-        }
-        return result
-    }
+const arrayo = ["java", "PHP", "fortan", "goLang"];
+const kv = arrayo.entries();
+for (let keys of kv) {
+    console.log(keys)   //0 1 2 3
 }
-
-
-let stack = new Stack()
-
-console.log(stack.reverse("Sagar"))
