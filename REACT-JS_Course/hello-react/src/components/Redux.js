27a17bfdaf708bad630de4b37764613464e6a650
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-17 23:19:47 +0530

    redux cart component

---
+++ REACT-JS_Course/hello-react/src/components/Redux.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import sc from './shopping-cart.png'
+
+function Redux(props) {
+    console.warn(`Redux prop data: ${props}`)
+    return (
+        <div>
+            <h1>Redux Component</h1>
+            <div className="add-to-cart">
+                <img src={sc} alt="" set="" />
+            </div>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                    <img src="https://www.tesla.com/sites/default/files/images/roadster/roadster-social.jpg" alt="" srcset="" />
+                </div>
+                <div className="text-wrapper item">
+                    <span>
+                        Roadster
+                    </span>
+                    <span>
+                        Price: $50000
+                    </span>
+                </div>
+                <div className="btn-wrapper item">
+                    <button onClick={() => props.addToCartHandler({ price: 80000, name: "TeslaModel S" })}>Add to Cart</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Redux
