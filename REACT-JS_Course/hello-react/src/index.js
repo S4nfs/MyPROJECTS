51e33244e7c5cebbeee5195f9a132db08917d2b3
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-17 23:22:05 +0530

    creating global state

--- REACT-JS_Course/hello-react/src/index.js
+++ REACT-JS_Course/hello-react/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux'
+import rootReducer from './services/reducers'
+// import { configureStore } from '@reduxjs/toolkit'
+const store = createStore(rootReducer);
+console.warn(`store data : ${store}`)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
