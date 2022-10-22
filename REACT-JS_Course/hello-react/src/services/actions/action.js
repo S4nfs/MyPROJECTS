be42c6216b63b8b5098c9dfb1e919ee2fde1305a
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-22 20:04:55 +0530

    no defining of constants in seperate module

--- REACT-JS_Course/hello-react/src/services/actions/action.js
+++ REACT-JS_Course/hello-react/src/services/actions/action.js
@@ -1,16 +1,14 @@
-import { ADD_TO_CART } from '../constants'
 
 export const addToCart = (data) => {
-    console.warn(`Redux Cart Data: ${data}`)
+    // console.warn(`Redux Cart Data: ${data}`)
     return {
-        type: ADD_TO_CART,
+        type: "ADD_TO_CART",
         data: data
     }
 }
 
-// export const removeToCart = (data) => {
-//     return {
-//         type: 'REMOVE_TO_CART',
-//         data: data
-//     }
-// }
+export const removeToCart = () => {
+    return {
+        type: "REMOVE_TO_CART",
+    }
+}
