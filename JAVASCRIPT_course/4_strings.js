8c6a581681a00aea55c2d2776fe009f8d877e1e0
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-27 23:09:47 +0530

    output logs

--- JAVASCRIPT_course/4_strings.js
+++ JAVASCRIPT_course/4_strings.js
@@ -8,17 +8,18 @@ lastIndexOf() method -          : Returns the index of the last occurrence in th
 replace()                       : Replaces a specified value with another value in a string:
 concat()                        : The concat() method can be used instead of the plus operator
 includes()                      : Returns true if a string contains a specified value.
+search()                        : searches a string and returns the position of the match:
 */
 
 //✔️slice() method - Extract the part of string and return that part in a new string (0, -2)
 var str = "Apple, Banana, Orange";
 let res = str.slice(0, 4);
-console.log(res);                     //Output: Appl not 'e' as it does not include end | there is no splice method for strings
+console.log(res);                           //Output: Appl not 'e' as it does not include end | there is no splice method for strings
 
 //✔️substring(start,end) - Same as slice but you can't specify nagative index at the end parameter (0, 4) or it will swap the values autmatically
 var str = "Apple, Banana, Orange";
 let res1 = str.substring(0, 4);
-console.log(res1);                    //Appl
+console.log(res1);                          //Appl
 
 /*Difference in slice and substring
 1. In case of substring() it swaps the arguments when start>stop where the slice() returns the empty string
@@ -28,31 +29,35 @@ console.log(res1);                    //Appl
 
 //✔️trim() method - Remove whitespaces from both sides of a string
 var strr = "         Hello World        ";
-console.log(strr.trim());
+console.log(strr.trim());                   //Hello World
 
 //✔️split() method - Convert string into an array
 var strr = "a,b,c,d,e,f";
-console.log(strr.split(","));
+console.log(strr.split(","));               //[ 'a', 'b', 'c', 'd', 'e', 'f' ]
 
 //✔️join() method  - Convert array into string 
 var arr = ["a", "b", "c", "d", "e", "f"];
-console.log(arr.join("-"));
+console.log(arr.join("-"));                 //a-b-c-d-e-f
 
 //✔️indexOf() method - Returns the index of the first occurrence in the array or -1 if not found
 let text = "Hello world, welcome to the universe.";
-console.log(text.indexOf("w"));        //Output: 6
+console.log(text.indexOf("w"));            //6
 
 //✔️lastIndexOf() method -  returns the index of the last occurrence of a specified text in a string
 let loc = "Please locate where 'locate' occurs!";
-console.log(loc.lastIndexOf("locate"));
+console.log(loc.lastIndexOf("locate"));    //21
 
 //✔️replace() - Replaces a specified value with another value in a string:
 let txt = "I am Sagar Verma";
 console.log(txt.replace("Sagar", "Ajay"));  //To replace all matches, use rgx /Sagar/g flag (global match):
 
-//✔️concat() - The concat() method can be used instead of the plus operator
+//✔️concat() - similar to plus operator
 console.log("Hello".concat(" ", "World!"));
 
-//✔️includes() - Returns true if a string contains a specified value.
-let t = "Hello world, welcome to the universe.";
-console.log(text.includes("world"));
+//✔️includes() - returns true if a string contains a specified value.
+let te = "Hello world, welcome to the universe.";
+console.log(te.includes("world"));          //true
+
+//✔️search()  - method searches a string for a specified value and returns the index position of the match:
+let t = "Visit Lucknow!";
+console.log(t.search("Lucknow"));         //6
