5a5bd75ad15b0792c51ac99b27269901efca12be
S4nfs <zyzzaestheticsgods@gmail.com>
2022-10-31 22:38:05 +0530

    function scope  - Hoisting

--- JAVASCRIPT_course/2_Let,Var,Const.js
+++ JAVASCRIPT_course/2_Let,Var,Const.js
@@ -1,13 +1,13 @@
 //Example: 1 Hoisting 🍄-----------------------------------
-//Hoisting is JavaScript's default behavior of moving declarations to the top. Var is hoisted but let is not 
-"use strict";                           //means no hoisting rules
+//Hoisting is JavaScript's default behavior of moving all variable and function declarations to the top. Var is hoisted but let & const are not 
+"use strict";                           // means no hoisting rules
 function receive(name = "") {
     if (name == "Sagar") {
-        var x = "ok";                    //u need to proper define variables
+        var x = "ok";                   // u need to proper define variables
     } else {
         var x = "not ok";
     }
-    var x;    // x is hoisted to the top //Declaration
+    var x;                              // x is hoisted to the top //Declaration
     console.log(x)
 }
 receive("Sagar");
@@ -22,5 +22,19 @@ if (a == 1) {
     console.log(a); //10
     console.log(b); //20 but its block scoped
 }
-console.log(a); //10
+console.log(a); //10 but u can't access it outside if it was inside a function scope
 console.log(b); //2
+
+//Example: 3 In functions 🍄--------------------------------------
+carDetails();                           // perfectly works
+function carDetails() {                 // function declaration is moved to the top of the global scope
+    var model = "AUDI";
+    console.log(model);
+}
+
+//
+carDetails();                           // Prints “TypeError : carDetails is not a function”
+var carDetails = function () {          // This is a function expression and therefore not hoisted
+    var model = "AUDI";
+    console.log(model);
+}
