093d288deca1762af3c7dadaf62dc7dab708ab49
S4nfs <zyzzaestheticsgods@gmail.com>
2022-11-12 21:55:30 +0530

    types recursion

--- JAVASCRIPT_course/11_Recursion.js
+++ JAVASCRIPT_course/11_Recursion.js
@@ -1,12 +1,12 @@
 //Recursion is when a function calls itself. Its basically a loop but with functions.
-let num = 0;
-function show() {
-    console.log("Hi", num);
-    num++;
-        show();                     //Recursion
-    
-}
-show();
+// let num = 0;
+// function show() {
+//     console.log("Hi", num);
+//     num++;
+//     show();                             //Recursion (infinite)
+
+// }
+// show();
 
 // let num = 0;
 // function show() {
@@ -17,3 +17,24 @@ show();
 //     }
 // }
 // show();
+
+//Find Factorial of a number 4
+// const f = n => n <= 1 ? 1 : n * f(n - 1);
+// console.log(f(4))                       //4*3*2*1 = 24
+
+
+//Indirect Recursion (functions calling in each others hood )
+let item = 0, money = 100;
+function addToCart(x) {
+    if (x > 0) {
+        console.log("I have ", item, "item & Balance Rs ", x);
+        balance(x);
+    } else {
+        console.log("Cart", item, "Balance left: Rs", x);
+    }
+}
+function balance(x) {
+    item++;
+    addToCart(x - 10)
+}
+addToCart(money)
