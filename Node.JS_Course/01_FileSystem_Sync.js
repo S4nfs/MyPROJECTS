d310db9e228114e40d63256d0822d13f97615b2a
S4nfs <zyzzaestheticsgods@gmail.com>
2022-11-17 20:18:24 +0530

    file system updated

--- Node.JS_Course/01_FileSystem_Sync.js
+++ Node.JS_Course/01_FileSystem_Sync.js
@@ -1,7 +1,26 @@
+/*
+fs.readFile(fileName [,options], callback) 	        - read the physical file asynchronously options- default encoding is utf8 flag -"r"
+fs.writeFile(filename, data[, options], callback)   - Writes to the file. If file exists then overwrite the content otherwise creates new file.
+fs.open(path, flags[, mode], callback) 	            - Opens file for reading or writing.
+fs.rename(oldPath, newPath, callback) 	            - Renames an existing file.
+fs.chown(path, uid, gid, callback) 	                - Asynchronous chown.
+fs.stat(path, callback) 	                        - Returns fs.stat object which includes important file statistics.
+fs.link(srcpath, dstpath, callback) 	            - Links file asynchronously.
+fs.symlink(destination, path[, type], callback) 	- Symlink asynchronously.
+fs.rmdir(path, callback) 	                        - Renames an existing directory.
+fs.mkdir(path[, mode], callback) 	                - Creates a new directory.
+fs.readdir(path, callback) 	                        - Reads the content of the specified directory.
+fs.utimes(path, atime, mtime, callback) 	        - Changes the timestamp of the file.
+fs.exists(path, callback) 	                        - Determines whether the specified file exists or not.
+fs.access(path[, mode], callback) 	                - Tests a user's permissions for the specified file.
+fs.appendFile(file, data[, options], callback) 	    - Appends new content to the existing file. 
+*/
+
+
 const fs = require("fs"); //file system
 
-//✔️ create a file 
-// fs.writeFileSync("Node.jS_Course/readmeFromNodeJS.txt", "Hello World!");     //overwrites the content      
+//✔️ create a file
+// fs.writeFileSync("Node.jS_Course/readmeFromNodeJS.txt", "Hello World!");     //overwrites the content
 
 //✔️ append content to a file without overwriting the previous content
 // fs.appendFileSync("Node.jS_Course/readmeFromNodeJS.txt", " I am learning Node.js");
@@ -24,13 +43,13 @@ const fs = require("fs"); //file system
 6.Delete both file and folder
 */
 
-// fs.mkdirSync("Node.JS_Course/NodeJSFileSystemCRUD");                       
+// fs.mkdirSync("Node.JS_Course/NodeJSFileSystemCRUD");
 
-// fs.writeFileSync("Node.JS_Course/NodeJSFileSystemCRUD/Data.txt", "Hello World");
+// fs.writeFile("Node.JS_Course/NodeJSFileSystemCRUD/Data.txt", "Hello World");
 
 // fs.appendFileSync("Node.JS_Course/NodeJSFileSystemCRUD/Data.txt", " I am learning Node.js");
 
-// const mybufferdata = fs.readFileSync("Node.JS_Course/NodeJSFileSystemCRUD/Data.txt" , "utf8");
+// const mybufferdata = fs.readFileSync("Data.txt", "utf8");
 // console.log(mybufferdata);
 
 // fs.renameSync("Node.JS_Course/NodeJSFileSystemCRUD/Data.txt", "Node.JS_Course/NodeJSFileSystemCRUD/Data_new.txt");
