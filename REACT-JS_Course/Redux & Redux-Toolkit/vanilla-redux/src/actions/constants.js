19954a09017ccfade1872c54586f04a2d0a98475
S4nfs <zyzzaestheticsgods@gmail.com>
2022-12-23 22:51:36 +0530

    constants for actions and reducers

---
+++ REACT-JS_Course/Redux & Redux-Toolkit/vanilla-redux/src/actions/constants.js
@@ -0,0 +1,10 @@
+export const countConstants = {
+    ADD: "ADD",
+    SUBTRACT: "SUBTRACT",
+    RESET: "RESET",
+    ADD_NUMBER: "ADD_NUMBER"
+}
+
+export const authConstants = {
+    TOGGLE_AUTH: "TOGGLE_AUTH"
+}
