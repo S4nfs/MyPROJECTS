d9b4ef31f4c12f9ba35a0273305804c4f0485386
S4nfs <zyzzaestheticsgods@gmail.com>
2022-12-30 23:57:21 +0530

    auth slice

---
+++ REACT-JS_Course/Redux_Redux-Toolkit/reduxToolkit/src/slice/auth.slice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const authSlice = createSlice({  //combine reducers and action using createSlice
+    name: "auth",
+    initialState: {
+        isLoggedIn: true,
+    },
+    reducer: {
+        //actions
+        TOGGLE_AUTH: (state) => {
+            state.isLoggedIn = !state.isLoggedIn
+        },
+
+    }
+})
+
+export const { TOGGLE_AUTH } = authSlice.actions;
+export default authSlice.reducer;
