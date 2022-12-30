7d9d6911e5e254b61208cf6624a48d00c5b27ff0
S4nfs <zyzzaestheticsgods@gmail.com>
2022-12-30 23:57:15 +0530

    count slice

---
+++ REACT-JS_Course/Redux_Redux-Toolkit/reduxToolkit/src/slice/count.slice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const counterSlice = createSlice({  //combine reducers and action using createSlice
+    name: "counter",
+    initialState: {
+        count: 0
+    },
+    reducer: {
+        //actions
+        ADD: (state) => {
+            state.count = state.count + 1
+        },
+        SUBTRACT: (state) => {
+            state.count -= 1
+        },
+        RESET: (state) => {
+            state.count = 0
+        },
+        ADD_NUMBER: (state, action) => {
+            state.count += action.payload
+        },
+    }
+})
+
+export const { ADD, SUBTRACT, RESET, ADD_NUMBER } = counterSlice.actions
+export default counterSlice.reducer
