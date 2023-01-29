07b7bb6f1e3c989914d78abe6f658a9fb30f78f1
S4nfs <zyzzaestheticsgods@gmail.com>
2023-01-29 22:21:51 +0530

    db connect

---
+++ REACT-JS_Course/GraphQL/server/config/db.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const connectDB = async () => {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Database connected: ${conn.connection.host}`.cyan.underline.bold)
+}
+module.exports = connectDB
