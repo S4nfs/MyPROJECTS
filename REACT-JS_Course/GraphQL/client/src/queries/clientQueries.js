69e8ff7fda59fe338c1a9d87ee28d0612a7a5d13
S4nfs <zyzzaestheticsgods@gmail.com>
2023-02-28 23:32:00 +0530

    mutation and queries

---
+++ REACT-JS_Course/GraphQL/client/src/queries/clientQueries.js
@@ -0,0 +1,15 @@
+const { gql } = require("@apollo/client");
+
+const GET_CLIENTS = gql`
+query getClients{
+     
+   clients{
+        id
+        name
+        email
+        phone
+   }
+ 
+}`
+
+export { GET_CLIENTS }
