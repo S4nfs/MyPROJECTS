a9c328df47b7fc33aca6701df04e7c7f9ba4eab5
S4nfs <zyzzaestheticsgods@gmail.com>
2023-02-28 23:30:44 +0530

    delete and update project schema

--- REACT-JS_Course/GraphQL/server/schema/schema.js
+++ REACT-JS_Course/GraphQL/server/schema/schema.js
@@ -125,6 +125,43 @@ const myMutation = new GraphQLObjectType({
                 })
                 return project.save()
             }
+        },
+        deleteProject: {
+            type: ProjectType,
+            args: {
+                id: { type: GraphQLNonNull(GraphQLID) }
+            },
+            resolve(parent, args) {
+                return Project.findByIdAndRemove(args.id)
+            }
+        },
+        updateProject: {
+            type: ProjectType,
+            args: {
+                id: { type: GraphQLNonNull(GraphQLID) },
+                name: { type: GraphQLString },
+                description: { type: GraphQLString },
+                status: {
+                    type: new GraphQLEnumType({
+                        name: 'ProjectStatusUpdate',
+                        values: {
+                            new: { value: 'Not Started' },
+                            progress: { value: 'In Progress' },
+                            completed: { value: 'Completed' },
+                        },
+                    }),
+                },
+            },
+            resolve(parent, args) {
+                return Project.findByIdAndUpdate(args.id, {
+                    $set: {
+                        name: args.name,
+                        description: args.description,
+                        status: args.status
+                    },
+                },
+                    { new: true })
+            }
         }
     }
 })
