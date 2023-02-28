8267768b6dc796877edebb788dcb7547482db2e4
S4nfs <zyzzaestheticsgods@gmail.com>
2023-02-28 23:32:31 +0530

    nested mutation component

---
+++ REACT-JS_Course/GraphQL/client/src/components/ClientRow.jsx
@@ -0,0 +1,30 @@
+import { useMutation } from '@apollo/client'
+import React from 'react'
+import { FaTrash } from 'react-icons/fa'
+import { DELETE_CLIENT } from '../mutations/clientMutations'
+import { GET_CLIENTS } from '../queries/clientQueries'
+const ClientRow = ({ client }) => {
+    const [deleteClient] = useMutation(DELETE_CLIENT, {
+        variables: { id: client.id },
+
+        //reflecting back changes to cache or you can fetch queries again, we are using first method ...
+        update(cache, { data: { deleteClient } }) {
+            const { clients } = cache.readQuery({ query: GET_CLIENTS })
+            cache.writeQuery({
+                query: GET_CLIENTS,
+                data: { clients: clients.filter(client => client.id !== deleteClient.id) }
+            })
+        }
+        //.....
+    })
+    return (
+        <tr>
+            <td>{client.name}</td>
+            <td>{client.email}</td>
+            <td>{client.phone}</td>
+            <td><button className="btn btn-danger btn-sm" onClick={deleteClient}><FaTrash /></button></td>
+        </tr>
+    )
+}
+
+export default ClientRow
