00e16316dc8b7a4868d61229730d1fda1ebf85b4
S4nfs <zyzzaestheticsgods@gmail.com>
2023-03-02 23:37:16 +0530

    client info component

---
+++ REACT-JS_Course/GraphQL/client/src/components/ClientInfo.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { FaEnvelope, FaPhone, FaIdBadge } from 'react-icons/fa'
+const ClientInfo = ({ client }) => {
+    return (
+        <>
+            <h5 className="mt-5">Client Information</h5>
+            <ul className="list-group">
+                <li className="list-group-item">
+                    <FaIdBadge className='icon' />{client?.name}
+                </li>
+                <li className="list-group-item">
+                    <FaEnvelope className='icon' />{client?.email}
+                </li>
+                <li className="list-group-item">
+                    <FaPhone className='icon' />{client?.phone}
+                </li>
+            </ul>
+        </>
+    )
+}
+
+export default ClientInfo
