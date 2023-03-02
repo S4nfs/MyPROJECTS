7f9d52fd226841c3487cb30b99c7761f908fa5f5
S4nfs <zyzzaestheticsgods@gmail.com>
2023-03-02 23:35:35 +0530

    link to home

--- REACT-JS_Course/GraphQL/client/src/components/Header.jsx
+++ REACT-JS_Course/GraphQL/client/src/components/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import logo from './assets/logo.png'
 const Header = () => {
     return (
         <nav className='navbar bg-light mb-4 p-0'>
             <div className="container">
-                <a href="http://" className='navbar-brand'>
+                <Link to="/" className='navbar-brand'>
                     <div className="d-flex">
                         <img src={logo} alt="" srcset="" />
                         <div>GraphQl Project</div>
                     </div>
-                </a>
+                </Link>
             </div>
         </nav>
     )
