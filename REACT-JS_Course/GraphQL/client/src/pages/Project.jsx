1f72efab01263c11d9b414a3b351593b0b48a8a9
S4nfs <zyzzaestheticsgods@gmail.com>
2023-03-02 23:34:58 +0530

    project page component

---
+++ REACT-JS_Course/GraphQL/client/src/pages/Project.jsx
@@ -0,0 +1,34 @@
+import { useQuery } from '@apollo/client'
+import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import ClientInfo from '../components/ClientInfo'
+import DeleteProjectButton from '../components/DeleteProjectButton'
+import Spinner from '../components/Spinner'
+import { GET_PROJECT } from '../queries/projectQueries'
+
+const Project = () => {
+    const { id } = useParams()
+    const { data, loading, error } = useQuery(GET_PROJECT, { variables: { id } })
+
+    if (loading) return <Spinner />
+    if (error) return <p>Something Went Wrong ?</p>
+
+    return (
+        <>
+            {!loading && !error && (
+                <div className="mx-auto w-5 card p-5">
+                    <Link to='/' className="btn btn-light btn-sm w-25 d-inline ms-auto">Back</Link>
+                    <h1>{data.project.name}</h1>
+                    <p>{data.project.description}</p>
+                    <h5 className="mt-3">Status</h5>
+                    <p className="lead">{data.project.status}</p>
+                    <ClientInfo client={data.project.client}></ClientInfo>
+                    <EditProjectForm projectData={data.project} />
+                    <DeleteProjectButton projectId={data.project.id} />
+                </div>
+            )}
+        </>
+    )
+}
+
+export default Project
