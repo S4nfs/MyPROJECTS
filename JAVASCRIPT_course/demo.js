88f92de69fe0b2277cbb1643b5a1abe82da6edb8
S4nfs <zyzzaestheticsgods@gmail.com>
2023-03-02 23:37:02 +0530

    debug code no use

--- JAVASCRIPT_course/demo.js
+++ JAVASCRIPT_course/demo.js
@@ -1,66 +1,41 @@
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
+class ParkingSlot {
+  slots = []
+  constructor(parkingSize) {
+    this.slots = new Array(parkingSize).fill(null)
+  }
+
+  park(carId) {
+    if (this.slots.every((elem) => elem !== null)) {
+      return false
+    } else {
+      const getIndex = this.slots.indexOf(null)
+      if (getIndex !== -1) this.slots[getIndex] = carId
+      return true
+    }
+
+  }
+
+
+  getSlots() {
+    return ("Slots: ", this.slots)
+  }
+
+  remove(carId) {
+    if (this.slots.find((cardID) => cardID == carId) === undefined) return false
+    else {
+      this.slots.splice(this.slots.indexOf(carId), 1, null)
+      return true
+    }
   }
 }
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.size = 0
-  }
-
-  isEmpty() {
-    return this.size === 0
-  }
-  getSize() {
-    return this.size
-  }
-  prepend(value) {
-    const node = new Node(value)
-    if (this.isEmpty()) {
-      this.head = node
-    } else {
-      node.next = this.head;
-      this.head = node
-    }
-    this.size++
-  }
-  print() {
-    if (this.isEmpty()) {
-      console.log(`List is empty`)
-    } else {
-      let curr = this.head;
-      let listValues = ''
-      while (curr) {
-        listValues += `${curr.value} `
-        curr = curr.next;
-      }
-      console.log(listValues)
-    }
-  }
-  append(value) {
-    const node = new Node(value)
-    if (this.isEmpty()) {
-      this.head = node;
-    } else {
-      let pre = this.head;
-      while (pre.next) {
-        pre = pre.next
-      }
-      pre.next = node
-    }
-    this.size++
-  }
-}
-
-let Obj = new LinkedList()
-console.log(Obj.isEmpty())
-Obj.prepend(10)
-Obj.prepend(20)
-Obj.prepend(30)
-Obj.print()
-Obj.append(40)
-Obj.append(50)
-Obj.print()
+let obj = new ParkingSlot(5)
+console.log(obj.getSlots())
+console.log(obj.park("Car-10"))
+console.log(obj.park("Car-20"))
+console.log(obj.park("Car-30"))
+console.log(obj.park("Car-40"))
+console.log(obj.park("Car-50"))
+console.log(obj.getSlots())
+console.log(obj.remove("Car-50"))
+console.log(obj.getSlots())
