a3e4175d26d760ab3e9e6603cf37f679f924efff
S4nfs <zyzzaestheticsgods@gmail.com>
2023-03-03 14:24:14 +0530

    refetching both clients and projects

--- REACT-JS_Course/GraphQL/client/src/components/ClientRow.jsx
+++ REACT-JS_Course/GraphQL/client/src/components/ClientRow.jsx
@@ -3,19 +3,22 @@ import React from 'react'
 import { FaTrash } from 'react-icons/fa'
 import { DELETE_CLIENT } from '../mutations/clientMutations'
 import { GET_CLIENTS } from '../queries/clientQueries'
+import { GET_PROJECTS } from '../queries/projectQueries'
 const ClientRow = ({ client }) => {
     const [deleteClient] = useMutation(DELETE_CLIENT, {
         variables: { id: client.id },
 
         //reflecting back changes to cache or you can fetch queries again, we are using first method ...
-        update(cache, { data: { deleteClient } }) {
-            const { clients } = cache.readQuery({ query: GET_CLIENTS })
-            cache.writeQuery({
-                query: GET_CLIENTS,
-                data: { clients: clients.filter(client => client.id !== deleteClient.id) }
-            })
-        }
+        // update(cache, { data: { deleteClient } }) {
+        //     const { clients } = cache.readQuery({ query: GET_CLIENTS })
+        //     cache.writeQuery({
+        //         query: GET_CLIENTS,
+        //         data: { clients: clients.filter(client => client.id !== deleteClient.id) }
+        //     })
+        // }
         //.....
+
+        refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }] //updated both clients and projects
     })
     return (
         <tr>
