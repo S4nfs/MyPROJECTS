bc2ffe4a4fa60f80da58dcac74b13f8e3f3b183d
S4nfs <zyzzaestheticsgods@gmail.com>
2023-03-10 15:23:31 +0530

    graph completed

--- JAVASCRIPT_course/21_Graph.js
+++ JAVASCRIPT_course/21_Graph.js
@@ -51,11 +51,41 @@ class Graph {
         this.adjacencyList[vertex1].add(vertex2)
         this.adjacencyList[vertex2].add(vertex1)
     }
+
+    display() {
+        for (let vertex in this.adjacencyList) {
+            console.log(vertex + "->" + [...this.adjacencyList[vertex]])
+        }
+    }
+
+    hasEdge(vertex1, vertex2) {
+        console.log("Has Edge: ", this.adjacencyList[vertex1].has(vertex2) && this.adjacencyList[vertex2].has(vertex1))
+    }
+
+    removeEdge(vertex1, vertex2) {
+        console.log("Deleted Edge: ", this.adjacencyList[vertex1].delete(vertex2) && this.adjacencyList[vertex2].delete(vertex1))
+    }
+
+    removeVertex(vertex) {
+        if (!this.adjacencyList[vertex]) {
+            return
+        }
+        for (let adjacentVertex of this.adjacencyList[vertex]) {    //if edges found else delete vertex
+            this.removeEdge(vertex, adjacentVertex)
+        }
+        delete this.adjacencyList[vertex]
+    }
 }
 
+//Time Complexity O(1) except removeVertex O(n)
 const graph = new Graph()
 graph.addVertex("A")
 graph.addVertex("B")
 graph.addVertex("C")
 graph.addEdge("A", "B")
 graph.addEdge("B", "C")
+graph.display()
+graph.hasEdge("B", "C")
+// graph.removeEdge("B", "C")
+graph.removeVertex("B")
+graph.display()
