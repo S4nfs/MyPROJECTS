159877d7865e3c96c6ed76f958f8160a0111e5a9
S4nfs <zyzzaestheticsgods@gmail.com>
2023-03-26 22:37:27 +0530

    paytm interview question

--- JAVASCRIPT_course/QnA.js
+++ JAVASCRIPT_course/QnA.js
@@ -1,24 +1,83 @@
-//algorithm 2 private
-const hasCycle = () => {
-    //head = this.head
-    let set = new Set()
-    // for traversing, initialize current with head node
-    let current = head
+// //algorithm 2 private
+// const hasCycle = () => {
+//     //head = this.head
+//     let set = new Set()
+//     // for traversing, initialize current with head node
+//     let current = head
 
-    while (current) {
-        if (set.has(current)) {
-            // if duplication occurs, return true
-            return true
-        } else {
-            set.add(current)
+//     while (current) {
+//         if (set.has(current)) {
+//             // if duplication occurs, return true
+//             return true
+//         } else {
+//             set.add(current)
+//         }
+
+//         current = current.next
+//     }
+//     // traverse is completed, cycle not found
+//     return false
+// };
+
+// //head = current node head
+
+// console.log(hasCycle())
+
+//----------------------------------------===================================--------------------------------------------
+var assert = require('assert');
+
+function coinChange(coins, amount) {
+    // Each memo[i] is the least amount of coins
+    // that can make the value equal to the index value.
+    const memo = Array(amount + 1).fill(Infinity);
+    memo[0] = 0;
+
+    for (let i = 1; i <= amount; i++) {
+        for (const coin of coins) {
+            if (i - coin >= 0) {
+                memo[i] = Math.min(memo[i], memo[i - coin] + 1);
+            }
         }
-
-        current = current.next
     }
-    // traverse is completed, cycle not found
-    return false
-};
+    return memo[amount] === Infinity ? -1 : memo[amount];
+}
 
-//head = current node head
+try {
+    assert.equal(coinChange([2, 3, 5], 11), 3);
 
-console.log(hasCycle())
+    console.log('PASSED: ' + 'First Test: coinChange([2, 3, 5], 11)');
+} catch (err) {
+    console.log(err);
+}
+
+try {
+    assert.equal(coinChange([2, 3, 5, 7], 17), 3);
+
+    console.log('PASSED: ' + 'Second Test: coinChange([2, 3, 5, 7]');
+} catch (err) {
+    console.log(err);
+}
+
+try {
+    assert.equal(coinChange([2, 3, 7], 15), 4);
+
+    console.log('PASSED: ' + 'Third Test: coinChange([2, 3, 7], 15)');
+} catch (err) {
+    console.log(err);
+}
+
+try {
+    assert.equal(coinChange([3, 5], 7), -1);
+
+    console.log('PASSED: ' + 'Fourth Test: coinChange([3, 5], 7)');
+} catch (err) {
+    console.log(err);
+}
+
+try {
+    assert.equal(coinChange([2, 3, 5], 1), -1);
+
+    console.log('PASSED: ' + 'Fifth Test: coinChange([2, 3, 5]');
+} catch (err) {
+    console.log(err);
+}
