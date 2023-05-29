a25819fe68683275bc4ae906d84d1b5058ee96ee
S4nfs <zyzzaestheticsgods@gmail.com>
2023-05-29 23:17:27 +0530

    typo

--- PHP_Course/OOPs/4_abstractClass.php
+++ PHP_Course/OOPs/4_abstractClass.php
@@ -1,6 +1,6 @@
 <?php
-/* Abstraction menas showing the essential features and hiding the background details. U can achieve abstraction by using:
-Abstract Class and  Interface
+/* Abstraction means showing the essential features and hiding the background details. U can achieve abstraction by using:
+Abstract Class and Interface
 
 Abstract Class - is a class that contains empty methods as well as non-empty methods, have constructor and variables, but instanciation is not possible (meaning u cant create an object of that class which is helpful when u want that class to be untouched).
 */
