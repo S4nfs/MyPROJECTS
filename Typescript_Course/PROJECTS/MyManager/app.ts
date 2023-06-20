abf02d712b853f144c5ba105474a63e7cbf69548
S4nfs <zyzzaestheticsgods@gmail.com>
2023-06-20 23:41:22 +0530

    project manager app

---
+++ Typescript_Course/PROJECTS/MyManager/app.ts
@@ -0,0 +1,24 @@
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  constructor() {
+    this.templateElement = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById(
+      "project-input"
+    )! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
