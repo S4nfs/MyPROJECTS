9c3606d3a7ab6dfb531d80b2ee2b17766d8de345
S4nfs <zyzzaestheticsgods@gmail.com>
2023-06-22 22:15:28 +0530

    getting form inputs and use decorators to autobind

--- Typescript_Course/PROJECTS/MyManager/src/app.ts
+++ Typescript_Course/PROJECTS/MyManager/src/app.ts
@@ -1,17 +1,50 @@
+//autobind decorator
+
+function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
   constructor() {
     this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
-    this.hostElement = document.getElementById("project-input")! as HTMLDivElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
 
     const importedNode = document.importNode(this.templateElement.content, true);
 
-    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element = importedNode.firstElementChild as HTMLFormElement; //form
+    this.element.id = "user-input";
+    this.titleInputElement = this.element.querySelector("#title") as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector("#description") as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector("#people") as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
+  @AutoBind
+  private submitHandler(e: Event) {
+    e.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+  private configure() {
+    // this.element.addEventListener("submit", this.submitHandler.bind(this)); //binding to current class
+    this.element.addEventListener("submit", this.submitHandler);
+  }
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
