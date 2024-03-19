dfd34166b501dc8b164bcfb27fe1f8690001596d
Sagar Verma <zyzzaestheticsgods@gmail.com>
2024-03-19 23:46:52 +0530

    community posts component added

---
+++ Typescript_Course/PROJECTS/Blueeit/src/app/r/[slug]/page.tsx
@@ -0,0 +1,41 @@
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from '@/app/api/config'
+import MiniCreatePost from '@/components/MiniCreatePost'
+import { getAuthSession } from '@/lib/auth'
+import { db } from '@/lib/db'
+import { notFound } from 'next/navigation'
+import React from 'react'
+
+interface PageProps {
+  params: {
+    slug: string
+  }
+}
+
+const page = async ({ params }: PageProps) => {
+  const { slug } = params
+  const session = await getAuthSession()
+  const subreddit = await db.subreddit.findFirst({
+    where: { name: slug },
+    include: {
+      posts: {
+        include: {
+          author: true,
+          votes: true,
+          comments: true,
+          subreddit: true,
+        },
+        take: INFINITE_SCROLLING_PAGINATION_RESULTS,
+      },
+    },
+  })
+  if (!subreddit) return notFound()
+  return (
+    <>
+      <h1 className='font-bold text-3xl md:text-4xl h-14 '>r/{subreddit.name}</h1>
+      <MiniCreatePost session={session} />
+      {/* Show posts in users feed  */}
+    </>
+  )
+}
+
+export default page
