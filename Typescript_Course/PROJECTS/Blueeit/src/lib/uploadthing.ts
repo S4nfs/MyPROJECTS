0bb4892604f764ac7f7d58a3e6db7ac506141e6e
Sagar Verma <zyzzaestheticsgods@gmail.com>
2024-03-23 23:00:42 +0530

    uploadthing cloud storage added

---
+++ Typescript_Course/PROJECTS/Blueeit/src/lib/uploadthing.ts
@@ -0,0 +1,6 @@
+import { generateReactHelpers } from "@uploadthing/react/hooks";
+import { OurFileRouter } from "@/app/api/uploadthing/core";
+
+
+
+export const {uploadFiles} = generateReactHelpers<OurFileRouter>()
