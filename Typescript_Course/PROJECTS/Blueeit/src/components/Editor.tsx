516e0dde02b742c37213c985bf0506cd448d2830
Sagar Verma <zyzzaestheticsgods@gmail.com>
2024-03-30 00:00:48 +0530

    fixed bug regarding rendered more hooks than during the previous render

--- Typescript_Course/PROJECTS/Blueeit/src/components/Editor.tsx
+++ Typescript_Course/PROJECTS/Blueeit/src/components/Editor.tsx
@@ -27,10 +27,36 @@ const Editor = ({ subredditId }: EditorProps) => {
     },
   })
   const ref = useRef<EditorJS>()
-  const [isMounted, setIsMounted] = useState<boolean>(false)
-  const _titleRef = useRef<HTMLTextAreaElement>()
-  const pathName = usePathname()
+  const _titleRef = useRef<HTMLTextAreaElement>(null)
   const router = useRouter()
+  const [isMounted, setIsMounted] = useState<boolean>(false)
+  const pathname = usePathname()
+
+  const { mutate: createPost } = useMutation({
+    mutationFn: async ({ title, content, subredditId }: PostCreationRequest) => {
+      const payload: PostCreationRequest = { title, content, subredditId }
+      const { data } = await axios.post('/api/subreddit/post/create', payload)
+      return data
+    },
+    onError: () => {
+      return toast({
+        title: 'Something went wrong.',
+        description: 'Your post was not published. Please try again.',
+        variant: 'destructive',
+      })
+    },
+    onSuccess: () => {
+      // turn pathname /r/mycommunity/submit into /r/mycommunity
+      const newPathname = pathname.split('/').slice(0, -1).join('/')
+      router.push(newPathname)
+
+      router.refresh()
+
+      return toast({
+        description: 'Your post has been published.',
+      })
+    },
+  })
 
   const initializeEditor = useCallback(async () => {
     const EditorJS = (await import('@editorjs/editorjs')).default
@@ -46,17 +72,15 @@ const Editor = ({ subredditId }: EditorProps) => {
     if (!ref.current) {
       const editor = new EditorJS({
         holder: 'editor',
-        onReady: () => {
+        onReady() {
           ref.current = editor
         },
-        placeholder: 'Start writing...',
+        placeholder: 'Type here to write your post...',
         inlineToolbar: true,
-        data: {
-          blocks: [],
-        },
+        data: { blocks: [] },
         tools: {
           header: Header,
-          linktool: {
+          linkTool: {
             class: LinkTool,
             config: {
               endpoint: '/api/link',
@@ -67,7 +91,9 @@ const Editor = ({ subredditId }: EditorProps) => {
             config: {
               uploader: {
                 async uploadByFile(file: File) {
+                  // upload to uploadthing
                   const [res] = await uploadFiles([file], 'imageUploader')
+
                   return {
                     success: 1,
                     file: {
@@ -80,7 +106,7 @@ const Editor = ({ subredditId }: EditorProps) => {
           },
           list: List,
           code: Code,
-          InlineCode: InlineCode,
+          inlineCode: InlineCode,
           table: Table,
           embed: Embed,
         },
@@ -89,70 +115,11 @@ const Editor = ({ subredditId }: EditorProps) => {
   }, [])
 
   useEffect(() => {
-    const init = async () => {
-      await initializeEditor()
-      setTimeout(() => {
-        //set focus to title
-        _titleRef.current?.focus()
-      })
-    }
-    if (isMounted) {
-      init()
-      return () => {
-        ref.current?.destroy()
-        ref.current = undefined
-      }
-    }
-  }, [isMounted, initializeEditor])
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setIsMounted(true)
-    }
-  }, [])
-
-  const { mutate: createPost } = useMutation({
-    mutationFn: async ({ title, content, subredditId }: PostCreationRequest) => {
-      const payload: PostCreationRequest = { title, content, subredditId }
-      const { data } = await axios.post('/api/subreddit/post/create', payload)
-      return data
-    },
-    onError: () => {
-      return toast({
-        title: 'Something went wrong',
-        description: `Your post can't be publish, something went wrong. Please try again later.`,
-        variant: 'destructive',
-      })
-    },
-    onSuccess: () => {
-      const newPathName = pathName.split('/').slice(0, -1).join('/')
-      router.push(newPathName)
-      router.refresh()
-      toast({
-        title: 'Post created',
-        description: 'Your post has been published',
-        variant: 'success',
-      })
-    },
-  })
-
-  async function onSubmit(data: PostCreationRequest) {
-    const blocks = await ref.current?.save()
-    const payload: PostCreationRequest = {
-      title: data.title,
-      subredditId,
-      content: blocks,
-    }
-    createPost(payload)
-  }
-  if (!isMounted) {
-    return null
-  }
-  useEffect(() => {
     if (Object.keys(errors).length) {
       for (const [_key, value] of Object.entries(errors)) {
+        value
         toast({
-          title: 'Error',
+          title: 'Something went wrong.',
           description: (value as { message: string }).message,
           variant: 'destructive',
         })
@@ -160,7 +127,49 @@ const Editor = ({ subredditId }: EditorProps) => {
     }
   }, [errors])
 
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setIsMounted(true)
+    }
+  }, [])
+
+  useEffect(() => {
+    const init = async () => {
+      await initializeEditor()
+
+      setTimeout(() => {
+        _titleRef?.current?.focus()
+      }, 0)
+    }
+
+    if (isMounted) {
+      init()
+
+      return () => {
+        ref.current?.destroy()
+        ref.current = undefined
+      }
+    }
+  }, [isMounted, initializeEditor])
+
+  async function onSubmit(data: PostCreationRequest) {
+    const blocks = await ref.current?.save()
+
+    const payload: PostCreationRequest = {
+      title: data.title,
+      content: blocks,
+      subredditId,
+    }
+
+    createPost(payload)
+  }
+
+  if (!isMounted) {
+    return null
+  }
+
   const { ref: titleRef, ...rest } = register('title')
+
   return (
     <div className='w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200'>
       <form action='' id='subreddit-post-form' className='w-fit' onSubmit={handleSubmit(onSubmit)}>
