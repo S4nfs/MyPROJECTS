5fcbe59209eed3cd74474953582477a7d5841ac0
Sagar Verma <zyzzaestheticsgods@gmail.com>
2024-03-29 23:59:47 +0530

    infinite scrolling posts

--- Typescript_Course/PROJECTS/Blueeit/src/app/r/[slug]/page.tsx
+++ Typescript_Course/PROJECTS/Blueeit/src/app/r/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { INFINITE_SCROLLING_PAGINATION_RESULTS } from '@/app/api/config'
 import MiniCreatePost from '@/components/MiniCreatePost'
+import PostFeed from '@/components/PostFeed'
 import { getAuthSession } from '@/lib/auth'
 import { db } from '@/lib/db'
 import { notFound } from 'next/navigation'
@@ -34,6 +35,7 @@ const page = async ({ params }: PageProps) => {
       <h1 className='font-bold text-3xl md:text-4xl h-14 '>r/{subreddit.name}</h1>
       <MiniCreatePost session={session} />
       {/* Show posts in users feed  */}
+      <PostFeed initialPosts={} />
     </>
   )
 }
---
+++ Typescript_Course/PROJECTS/Blueeit/src/components/PostFeed.tsx
@@ -0,0 +1,50 @@
+import { ExtendedPost } from '@/types/db'
+import React, { useRef } from 'react'
+import { useIntersection } from '@mantine/hooks'
+import { useInfiniteQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from '@/app/api/config'
+import { useSession } from 'next-auth/react'
+
+interface PostFeedProps {
+  initialPosts: ExtendedPost[]
+  subredditName?: string
+}
+const PostFeed = ({ initialPosts, subredditName }: PostFeedProps) => {
+  const lastPostRef = useRef<HTMLElement>(null)
+  const { ref, entry } = useIntersection({
+    root: lastPostRef.current,
+    threshold: 1,
+  })
+  const { data: session } = useSession()
+  const { data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery(
+    ['infinite-query'],
+    async ({ pageParam = 1 }) => {
+      const query = `/api/posts?limit=${INFINITE_SCROLLING_PAGINATION_RESULTS}&page=${pageParam}` + (!!subredditName ? `&subredditName=${subredditName}` : '')
+      const { data } = await axios.get(query)
+      return data as ExtendedPost[]
+    },
+    {
+      getNextPageParam: (_, pages) => {
+        return pages.length + 1
+      },
+      initialData: { pages: [initialPosts], pageParams: [1] },
+    }
+  )
+  const posts = data?.pages.flatMap((page) => page) ?? initialPosts
+  return (
+    <ul className='flex flex-col col-span-2 skew-y-6'>
+      {posts.map((post, index) => {
+        const voteAmt = post.votes.reduce((acc, vote) => {
+          if (vote.type === 'UP') return acc + 1
+          if (vote.type === 'DOWN') return acc - 1
+          return acc
+        }, 0)
+        const currentVote = post.votes.find((vote) => vote.userId === session?.user.id)
+        return <div></div>
+      })}
+    </ul>
+  )
+}
+
+export default PostFeed
