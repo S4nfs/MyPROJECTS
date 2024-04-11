3ff78509835d02222c2657343bd77e84f4aa1fb0
Sagar Verma <zyzzaestheticsgods@gmail.com>
2024-04-11 23:24:48 +0530

    readLine nodejs

--- Node.JS_Course/10_Stream&Buffer.js
+++ Node.JS_Course/10_Stream&Buffer.js
@@ -1,18 +1,17 @@
 //Streams: Used to process(read and write) data piece by piece (chunks) therefore without keeping all the data in memory
 
-const fs = require("fs");
-const http = require("http");
+const fs = require('fs')
+const http = require('http')
 
-const server = http.createServer();
+const server = http.createServer()
 
-server.on("request", (req, res) => {
+server.on('request', (req, res) => {
   // ==========================================================================================================
   //1st Method Normal Way
   // fs.readFile("Stream&Buffer.txt", (err, data) => {
   //   if (err) return console.error(err);
   //   res.end(data.toString());
   // });
-
 
   // ==========================================================================================================
   //2nd Method Using Stream
@@ -28,11 +27,10 @@ server.on("request", (req, res) => {
   //   console.log(err);
   // });
 
-
   // ==========================================================================================================
   //3rd Method Using pipe(realtime data)  - Readble stream is much faster than sending the writeable reaponse to users thus forming "Backpressure" when response sent to data cannot be as fast as it is receiveing it from file, pipe fixes this problem by automatically leveraging the incoming and outgoing speed
 
-  // const rstream = fs.createReadStream("Stream&Buffer.txt");
-  // rstream.pipe(res);
-});
-server.listen(3000, "127.0.0.1");
+  const rstream = fs.createReadStream('Node.jS_Course/Stream&Buffer.txt')
+  rstream.pipe(res)
+})
+server.listen(3000, '127.0.0.1') //see http://localhost:3000 in browser
