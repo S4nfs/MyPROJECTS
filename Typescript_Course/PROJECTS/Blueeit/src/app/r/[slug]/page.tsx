4aa82d3077637ff33cfa48e759d5c179431f980a
S4NFS <zyzzaestheticsgods@gmail.com>
2024-07-26 23:42:53 +0530

    refactored

--- Typescript_Course/PROJECTS/Blueeit/src/app/r/[slug]/page.tsx
+++ Typescript_Course/PROJECTS/Blueeit/src/app/r/[slug]/page.tsx
@@ -25,6 +25,9 @@ const page = async ({ params }: PageProps) => {
           comments: true,
           subreddit: true,
         },
+        orderBy: {
+          createdAt: 'desc',
+        },
         take: INFINITE_SCROLLING_PAGINATION_RESULTS,
       },
     },
--- Typescript_Course/PROJECTS/Blueeit/src/components/EditorOutput.tsx
+++ Typescript_Course/PROJECTS/Blueeit/src/components/EditorOutput.tsx
@@ -1,3 +1,4 @@
+'use client'
 import dynamic from 'next/dynamic'
 import Image from 'next/image'
 import React from 'react'
