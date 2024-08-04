bed0aadaff3bf70856078f80b63036ce24fe530f
S4NFS <zyzzaestheticsgods@gmail.com>
2024-08-04 23:25:52 +0530

    searchBar component updated

--- Typescript_Course/PROJECTS/Blueeit/src/components/SearchBar.tsx
+++ Typescript_Course/PROJECTS/Blueeit/src/components/SearchBar.tsx
@@ -1,76 +1,89 @@
 'use client'
-import { FC, useCallback, useState } from 'react'
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/components/ui/Command'
+
+import { Prisma, Subreddit } from '@prisma/client'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
-import { Prisma, Subreddit } from '@prisma/client'
-import { useRouter } from 'next/navigation'
-import { Users } from 'lucide-react'
 import debounce from 'lodash.debounce'
+import { usePathname, useRouter } from 'next/navigation'
+import { FC, useCallback, useEffect, useRef, useState } from 'react'
+
+import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/components/ui/Command'
+import { useOnClickOutside } from '@/hooks/use-on-click-outside'
+import { Users } from 'lucide-react'
 
 interface SearchBarProps {}
 
 const SearchBar: FC<SearchBarProps> = ({}) => {
   const [input, setInput] = useState<string>('')
-  const { data: queryResults, refetch, isFetched, isFetching } = useQuery({
+  const pathname = usePathname()
+  const commandRef = useRef<HTMLDivElement>(null)
+  const router = useRouter()
+
+  useOnClickOutside(commandRef, () => {
+    setInput('')
+  })
+
+  const request = debounce(async () => {
+    refetch()
+  }, 300)
+
+  const debounceRequest = useCallback(() => {
+    request()
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const { isFetching, data: queryResults, refetch, isFetched } = useQuery({
     queryFn: async () => {
       if (!input) return []
       const { data } = await axios.get(`/api/search?q=${input}`)
-      return data as (Subreddit & { _count: Prisma.SubredditCountOutputType })[]
+      return data as (Subreddit & {
+        _count: Prisma.SubredditCountOutputType
+      })[]
     },
     queryKey: ['search-query'],
     enabled: false,
   })
 
-  const request = debounce(() => {
-    refetch()
-  })
-  //Debounce request To avoid spam the database in every keystorke as we search
-  const debounceRequest = useCallback(() => {
-    request()
-  }, [])
+  useEffect(() => {
+    setInput('')
+  }, [pathname])
 
-  const router = useRouter()
   return (
-    <Command className='relative rounded-lg border max-w-lg z-50 overflow-visible'>
+    <Command ref={commandRef} className='relative rounded-lg border max-w-lg z-50 overflow-visible'>
       <CommandInput
-        className='outline-none border-none focus:border-none focus:outline-none ring-0 text-gray-800'
-        placeholder='Search Communities...'
-        value={input}
+        isLoading={isFetching}
         onValueChange={(text) => {
           setInput(text)
           debounceRequest()
         }}
+        value={input}
+        className='outline-none border-none focus:border-none focus:outline-none ring-0'
+        placeholder='Search communities...'
       />
-      {input.length > 0 ? (
-        <CommandList className='absolute bg-white top-full inset-0 shadow rounded-b-md'>
+
+      {input.length > 0 && (
+        <CommandList className='absolute bg-white top-full inset-x-0 shadow rounded-b-md'>
           {isFetched && <CommandEmpty>No results found.</CommandEmpty>}
           {(queryResults?.length ?? 0) > 0 ? (
             <CommandGroup heading='Communities'>
               {queryResults?.map((subreddit) => (
                 <CommandItem
-                  key={subreddit.id}
-                  value={subreddit.name}
                   onSelect={(e) => {
                     router.push(`/r/${e}`)
                     router.refresh()
                   }}
+                  key={subreddit.id}
+                  value={subreddit.name}
                 >
-                  <Users className='mr-2 h-4 w-4' />{' '}
-                  <a
-                    href={
-                      `/r/${subreddit.name}` // Link to subreddit page
-                    }
-                    className='flex items-center'
-                  >
-                    r/{subreddit.name}{' '}
-                  </a>
+                  <Users className='mr-2 h-4 w-4' />
+                  <a href={`/r/${subreddit.name}`}>r/{subreddit.name}</a>
                 </CommandItem>
               ))}
             </CommandGroup>
           ) : null}
         </CommandList>
-      ) : null}
+      )}
     </Command>
   )
 }
