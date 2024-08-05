418035c6cc4effad3390ea95fe5e7a5b40fbf3b7
S4NFS <zyzzaestheticsgods@gmail.com>
2024-08-05 23:35:29 +0530

    Change Username API and component

--- Typescript_Course/PROJECTS/Blueeit/src/components/ui/UserAccountNav.tsx
+++ Typescript_Course/PROJECTS/Blueeit/src/components/ui/UserAccountNav.tsx
@@ -20,7 +20,7 @@ const UserAccountNav: FC<UserAccountNavProps> = ({ user }) => {
         <div className='flex items-center justify-start gap-2 p-2'>
           <div className='flex flex-col space-y-1 leading-none'>
             {user.name && <p className='font-medium'>{user.name}</p>}
-            {user.email && <p className='w-[200px] truncate text-sm text-zinc-700'>{user.email}</p>}
+            {user.email && <p className='w-[200px] truncate text-sm text-muted-foreground'>{user.email}</p>}
           </div>
         </div>
         <DropdownMenuSeparator />
